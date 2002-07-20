774bee835dbaa64f9066f1eb3ac666d13e915ef9
awilliams <awilliams@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2002-07-20 16:21:05 +0000

    *** empty log message ***
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@32 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.ui/src/org/rubypeople/rdt/internal/ui/text/ruby/RubyCompletionProcessor.java
+++ org.rubypeople.rdt.ui/src/org/rubypeople/rdt/internal/ui/text/ruby/RubyCompletionProcessor.java
@@ -1,14 +1,20 @@
 package org.rubypeople.rdt.internal.ui.text.ruby;
 
+import java.text.MessageFormat;
+
 import org.eclipse.jface.text.ITextViewer;
+import org.eclipse.jface.text.TextPresentation;
 import org.eclipse.jface.text.contentassist.CompletionProposal;
+import org.eclipse.jface.text.contentassist.ContextInformation;
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
 import org.eclipse.jface.text.contentassist.IContentAssistProcessor;
 import org.eclipse.jface.text.contentassist.IContextInformation;
+import org.eclipse.jface.text.contentassist.IContextInformationPresenter;
 import org.eclipse.jface.text.contentassist.IContextInformationValidator;
 
 public class RubyCompletionProcessor implements IContentAssistProcessor {
 	public static String[] proposals;
+	protected IContextInformationValidator contextInformationValidator = new RubyContextInformationValidator();
 
 	public RubyCompletionProcessor() {
 		super();
@@ -16,16 +22,17 @@ public class RubyCompletionProcessor implements IContentAssistProcessor {
 
 	public ICompletionProposal[] computeCompletionProposals(ITextViewer viewer, int documentOffset) {
 		String[] completionProposals = getDefaultProposals();
-		ICompletionProposal[] result= new ICompletionProposal[completionProposals.length];
-		for (int i= 0; i < completionProposals.length; i++) {
-			result[i]= new CompletionProposal(completionProposals[i], documentOffset, completionProposals[i].length(), documentOffset);
+		ICompletionProposal[] result = new ICompletionProposal[completionProposals.length];
+		for (int i = 0; i < completionProposals.length; i++) {
+			IContextInformation info = new ContextInformation(completionProposals[i], MessageFormat.format("{0} some context information.", new Object[] { completionProposals[i] }));
+			result[i] = new CompletionProposal(completionProposals[i], documentOffset, 0, completionProposals[i].length(), null, completionProposals[i], info, MessageFormat.format("Ruby keyword: {0}", new Object[] { completionProposals[i] }));
 		}
 		return result;
 	}
 
 	protected String[] getDefaultProposals() {
 		if (proposals == null) {
-			proposals = new String[] { "class", "def", "end"};
+			proposals = new String[] { "class", "def", "end" };
 		}
 
 		return proposals;
@@ -40,15 +47,39 @@ public class RubyCompletionProcessor implements IContentAssistProcessor {
 	}
 
 	public char[] getContextInformationAutoActivationCharacters() {
-		return null;
+		return new char[] { '#' };
 	}
 
 	public IContextInformationValidator getContextInformationValidator() {
-		return null;
+		return contextInformationValidator;
 	}
 
 	public String getErrorMessage() {
 		return null;
 	}
 
+	protected class RubyContextInformationValidator implements IContextInformationValidator, IContextInformationPresenter {
+		protected int installDocumentPosition;
+
+		/**
+		 * @see org.eclipse.jface.text.contentassist.IContextInformationPresenter#install(IContextInformation, ITextViewer, int)
+		 */
+		public void install(IContextInformation info, ITextViewer viewer, int documentPosition) {
+			installDocumentPosition = documentPosition;
+		}
+
+		/**
+		 * @see org.eclipse.jface.text.contentassist.IContextInformationValidator#isContextInformationValid(int)
+		 */
+		public boolean isContextInformationValid(int documentPosition) {
+			return Math.abs(installDocumentPosition - documentPosition) < 1;
+		}
+
+		/**
+		 * @see org.eclipse.jface.text.contentassist.IContextInformationPresenter#updatePresentation(int, TextPresentation)
+		 */
+		public boolean updatePresentation(int documentPosition, TextPresentation presentation) {
+			return false;
+		}
+	}
 }
