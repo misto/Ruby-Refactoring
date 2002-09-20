3a2d1434bddcbb2ebd5394f4ec15ba1ed7d6228e
awilliams <awilliams@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2002-09-20 01:28:16 +0000

    *** empty log message ***
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@54 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

---
+++ org.rubypeople.rdt.ui.tests/src/org/rubypeople/rdt/internal/ui/tests/TC_RubyViewerFilter.java
@@ -0,0 +1,27 @@
+package org.rubypeople.rdt.internal.ui.tests;
+
+import junit.framework.TestCase;
+
+import org.eclipse.core.internal.resources.Workspace;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.rubypeople.rdt.core.RubyProject;
+import org.rubypeople.rdt.internal.ui.RubyViewerFilter;
+
+public class TC_RubyViewerFilter extends TestCase {
+
+	public TC_RubyViewerFilter(String name) {
+		super(name);
+	}
+
+	public void testSelect() {
+		RubyViewerFilter filter = new RubyViewerFilter();
+		
+		IWorkspace workspace = new Workspace();
+		IWorkspaceRoot root = workspace.getRoot();
+		IProject project = root.getProject("ProjectOne");
+		RubyProject rubyProject = new RubyProject();
+//		filter.select(null, parentElement, element);
+	}
+}
