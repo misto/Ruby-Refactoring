d418b66207c4e64a39f344da2644eda046891f60
mbarchfe <mbarchfe@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2002-11-14 23:36:55 +0000

    TS_Debug added
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@76 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.tests.all/src/org/rubypeople/rdt/tests/all/TS_RdtAllTests.java
+++ org.rubypeople.rdt.tests.all/src/org/rubypeople/rdt/tests/all/TS_RdtAllTests.java
@@ -4,6 +4,7 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 
 import org.rubypeople.rdt.core.tests.core.TS_Core;
+import org.rubypeople.rdt.debug.core.tests.TS_Debug;
 import org.rubypeople.rdt.internal.debug.ui.tests.launcher.TS_Launcher;
 import org.rubypeople.rdt.internal.launching.tests.TS_Launching;
 import org.rubypeople.rdt.internal.ui.tests.TS_UiTests;
@@ -17,6 +18,7 @@ public class TS_RdtAllTests {
 		suite.addTest(TS_Launcher.suite());
 		suite.addTest(TS_Launching.suite());
 		suite.addTest(TS_UiTests.suite());
+		suite.addTest(TS_Debug.suite());
 		//$JUnit-END$
 		return suite;
 	}
