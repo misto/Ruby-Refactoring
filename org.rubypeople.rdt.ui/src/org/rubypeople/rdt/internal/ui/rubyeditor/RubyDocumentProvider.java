aae7da6bbde7a429210f2ffd6d68637992f1ad2f
mbarchfe <mbarchfe@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2002-11-15 21:10:25 +0000

    Added RubyAnnotationModel
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@87 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.ui/src/org/rubypeople/rdt/internal/ui/rubyeditor/RubyDocumentProvider.java
+++ org.rubypeople.rdt.ui/src/org/rubypeople/rdt/internal/ui/rubyeditor/RubyDocumentProvider.java
@@ -3,6 +3,8 @@ package org.rubypeople.rdt.internal.ui.rubyeditor;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IDocumentPartitioner;
+import org.eclipse.jface.text.source.IAnnotationModel;
+import org.eclipse.ui.IFileEditorInput;
 import org.eclipse.ui.editors.text.FileDocumentProvider;
 import org.rubypeople.rdt.internal.ui.RdtUiPlugin;
 import org.rubypeople.rdt.internal.ui.text.RubyTextTools;
@@ -16,11 +18,15 @@ public class RubyDocumentProvider extends FileDocumentProvider {
 	protected IDocument createDocument(Object element) throws CoreException {
 		IDocument document = super.createDocument(element);
 		if (document != null) {
-			RubyTextTools tools= RdtUiPlugin.getDefault().getTextTools();
-			IDocumentPartitioner partitioner= tools.createDocumentPartitioner();
+			RubyTextTools tools = RdtUiPlugin.getDefault().getTextTools();
+			IDocumentPartitioner partitioner = tools.createDocumentPartitioner();
 			document.setDocumentPartitioner(partitioner);
 			partitioner.connect(document);
 		}
 		return document;
+	}
+
+	protected IAnnotationModel createAnnotationModel(Object element) throws CoreException {
+		return new RubyAnnotationModel((IFileEditorInput) element);
 	}
 }
