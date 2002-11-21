c2afa46d7823fd2388186335c2f7f4ad8e5c007b
awilliams <awilliams@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2002-11-21 19:24:29 +0000

    *** empty log message ***
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@103 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

---
+++ org.rubypeople.rdt.core/src/org/rubypeople/rdt/core/RubyParsedComponent.java
@@ -0,0 +1,22 @@
+package org.rubypeople.rdt.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RubyParsedComponent {
+
+	public RubyParsedComponent() {
+		super();
+	}
+	
+	public List getChildren() {
+		List children = new ArrayList();
+		children.add(new Object());
+		return children;
+	}
+	
+	public String getName() {
+		return null;
+	}
+
+}
