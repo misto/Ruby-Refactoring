af6a6b23f254298510477b7b309af43d5602d988
mbarchfe <mbarchfe@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2003-01-06 21:10:09 +0000

    adds -I for directory "ruby"
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@152 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.launching/src/org/rubypeople/rdt/internal/launching/DebuggerRunner.java
+++ org.rubypeople.rdt.launching/src/org/rubypeople/rdt/internal/launching/DebuggerRunner.java
@@ -2,38 +2,48 @@ package org.rubypeople.rdt.internal.launching;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
+import org.eclipse.core.runtime.ILog;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.model.IProcess;
+import org.rubypeople.rdt.internal.debug.core.RdtDebugCorePlugin;
 import org.rubypeople.rdt.internal.debug.core.RubyDebuggerProxy;
 import org.rubypeople.rdt.internal.debug.core.model.RubyDebugTarget;
 
-
-
-
-
-
-/**
- * @author Administrator
- *
- * To change this generated comment edit the template variable "typecomment":
- * Window>Preferences>Java>Templates.
- * To enable and disable the creation of type comments go to
- * Window>Preferences>Java>Code Generation.
- */
 public class DebuggerRunner extends InterpreterRunner {
 
 	public IProcess run(InterpreterRunnerConfiguration configuration, ILaunch launch) {
-		IProcess process = super.run(configuration, launch) ;
-		RubyDebugTarget debugTarget = new RubyDebugTarget(launch, process);	
-		new RubyDebuggerProxy(debugTarget).start() ;			
-		launch.addDebugTarget(debugTarget);		
-		return process ;
-	}
-	
-	protected String getDebugCommandLineArgument() {
-		return " -reclipseDebug" ;	
+		IProcess process = super.run(configuration, launch);
+		RubyDebugTarget debugTarget = new RubyDebugTarget(launch, process);
+		new RubyDebuggerProxy(debugTarget).start();
+		launch.addDebugTarget(debugTarget);
+		return process;
 	}
 
+	protected String getDebugCommandLineArgument() {
+		return " -reclipseDebug";
+	}
+
+	protected String getDirectoryOfRubyDebuggerFile() {
+		// this class will usually reside in the launching.jar file ...
+		URL url = this.getClass().getResource("");
+		if (url != null && url.getProtocol().equals("jar")) {
+			String jarFile = url.getFile().substring(0, url.getFile().lastIndexOf("!"));
+			String dir = jarFile.substring(0, jarFile.lastIndexOf("/"));
+			return dir.replaceFirst("file:", "") + "/ruby";
+		}
+		// ... but in a runtime workbench the class has been loaded from bin directory
+		url = this.getClass().getResource("/");
+		if (url != null && url.getProtocol().equals("file")) {
+			return url.getFile() + "../ruby";
+		}
+		throw new RuntimeException("Could not find directory of ruby debugger file.") ;		
+	}
+
+	protected String renderLoadPath(InterpreterRunnerConfiguration configuration) {
+		return super.renderLoadPath(configuration) + " -I " + osDependentPath(this.getDirectoryOfRubyDebuggerFile().replace('/', File.separatorChar));
+	}
 }
