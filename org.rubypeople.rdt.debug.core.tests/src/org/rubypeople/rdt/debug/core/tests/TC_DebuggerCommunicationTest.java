150965064095b9ce9ea3bd1dfcb87d09998b0d2f
awilliams <awilliams@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2003-02-01 04:53:17 +0000

    Removed compile warnings.
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@179 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.debug.core.tests/src/org/rubypeople/rdt/debug/core/tests/TC_DebuggerCommunicationTest.java
+++ org.rubypeople.rdt.debug.core.tests/src/org/rubypeople/rdt/debug/core/tests/TC_DebuggerCommunicationTest.java
@@ -7,7 +7,6 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
 import org.rubypeople.rdt.internal.debug.core.ExceptionSuspensionPoint;
 import org.rubypeople.rdt.internal.debug.core.StepSuspensionPoint;
