4ab3649bcd59973d62e63651e62a17a156abb19f
cawilliams <cawilliams@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2004-03-04 01:14:59 +0000

    added library support methods (save, edit, load ruby libraries)
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@275 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.core/src/org/rubypeople/rdt/internal/core/RubyPlugin.java
+++ org.rubypeople.rdt.core/src/org/rubypeople/rdt/internal/core/RubyPlugin.java
@@ -1,20 +1,27 @@
 package org.rubypeople.rdt.internal.core;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.xml.parsers.SAXParserFactory;
+
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IAdapterManager;
-import org.eclipse.core.runtime.IPluginDescriptor;
-import org.eclipse.core.runtime.Platform;
-import org.eclipse.core.runtime.Plugin;
-import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.*;
 import org.rubypeople.rdt.core.RubyElement;
+import org.xml.sax.*;
 
 public class RubyPlugin extends Plugin {
+	private static final String CONFIGURATION_FILE = "libraryConfiguration.xml";
 	public final static String PLUGIN_ID = "org.rubypeople.rdt.core";
 	public final static String RUBY_NATURE_ID = PLUGIN_ID + ".rubynature";
 
+	private List libraries;
+	private RubyLibrary selectedLibrary;
+	
 	protected static RubyPlugin plugin;
 
 	public RubyPlugin(IPluginDescriptor descriptor) {
@@ -37,8 +44,134 @@ public class RubyPlugin extends Plugin {
 		manager.registerAdapters(new ResourceAdapterFactory(), IResource.class);
 	}
 
-	public static void log(RuntimeException runtimeException) {
-		getDefault().getLog().log(new Status(Status.INFO, RubyPlugin.PLUGIN_ID, 0, "", runtimeException));
+	public static void log(Exception runtimeException) {
+		getDefault().getLog().log(new Status(Status.INFO, RubyPlugin.PLUGIN_ID, 0, runtimeException.getMessage(), runtimeException));
+	}
+
+	/**
+	 * @return
+	 */
+	public RubyLibrary getSelectedLibrary() {
+		return selectedLibrary;
+	}
+
+	/**
+	 * @param installedInterpreters
+	 */
+	public void setInstalledLibraries(List installedInterpreters) {
+		libraries = installedInterpreters;		
+		if (installedInterpreters.size() > 0)
+			setSelectedLibrary((RubyLibrary)installedInterpreters.get(0));
+		else
+			setSelectedLibrary(null);
+	}
+
+	/**
+	 * @param library
+	 */
+	public void setSelectedLibrary(RubyLibrary library) {
+		selectedLibrary = library;
+		saveRuntimeConfiguration();
+	}
+
+	/**
+	 * @return
+	 */
+	public List getInstalledLibraries() {
+		if (libraries == null)
+			loadRuntimeConfiguration();
+		return libraries;
+	}
+	
+	protected void saveRuntimeConfiguration() {
+		writeXML(getRuntimeConfigurationWriter());
+	}
+
+	protected Writer getRuntimeConfigurationWriter() {
+		try {
+			OutputStream stream = new BufferedOutputStream(new FileOutputStream(getRuntimeConfigurationFile()));
+			return new OutputStreamWriter(stream);
+		} catch (FileNotFoundException e) {}
+
+		return null;
+	}
+	
+	protected void loadRuntimeConfiguration() {
+		libraries = new ArrayList();
+		try {
+			XMLReader reader = SAXParserFactory.newInstance().newSAXParser().getXMLReader();
+			reader.setContentHandler(getRuntimeConfigurationContentHandler());
+			Reader fileReader = this.getRuntimeConfigurationReader() ;
+			if (fileReader == null) {
+				return ;
+			}
+			reader.parse(new InputSource(fileReader)) ;
+		} catch(Exception e) {
+			RubyPlugin.log(e);
+		}
+	}
+
+	protected Reader getRuntimeConfigurationReader() {
+		try {
+			return new FileReader(getRuntimeConfigurationFile());
+		} catch(FileNotFoundException e) {			
+			return null ;
+		}
+	}
+	
+	protected void writeXML(Writer writer) {
+		try {
+			writer.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?><libraryconfig>");
+			Iterator librariesIterator = libraries.iterator();
+			while (librariesIterator.hasNext()) {
+				writer.write("<library name=\"");
+				
+				RubyLibrary entry = (RubyLibrary) librariesIterator.next();
+				writer.write(entry.getName());
+				writer.write("\" path=\"");
+				writer.write(entry.getInstallLocation().toString());
+				writer.write("\"");
+				if (entry.equals(selectedLibrary))
+					writer.write(" selected=\"true\"");
+				
+				writer.write("/>");
+			}
+			writer.write("</libraryconfig>");
+			writer.flush();
+		} catch(IOException e) {
+			RubyPlugin.log(e);
+		}
+	}
+
+	protected ContentHandler getRuntimeConfigurationContentHandler() {
+		return new ContentHandler() {
+			public void setDocumentLocator(Locator locator) {}
+			public void startDocument() throws SAXException {}
+			public void endDocument() throws SAXException {}
+			public void startPrefixMapping(String prefix, String uri) throws SAXException {}
+			public void endPrefixMapping(String prefix) throws SAXException {}
+			public void startElement(String namespaceURI, String localName, String qName, Attributes atts) throws SAXException {
+				if ("library".equals(qName)) {
+					String libraryName = atts.getValue("name");
+					IPath installLocation = new Path(atts.getValue("path"));
+					RubyLibrary library = new RubyLibrary(libraryName, installLocation);
+					libraries.add(library);
+					if (atts.getValue("selected") != null)
+						selectedLibrary = library;
+				}
+			}
+			public void endElement(String namespaceURI, String localName, String qName) throws SAXException {}
+			public void characters(char[] ch, int start, int length) throws SAXException {}
+			public void ignorableWhitespace(char[] ch, int start, int length) throws SAXException {}
+			public void processingInstruction(String target, String data) throws SAXException {}
+			public void skippedEntity(String name) throws SAXException {}
+		};
+	}
+	
+	protected File getRuntimeConfigurationFile() {
+		IPath stateLocation = RubyPlugin.getDefault().getStateLocation();
+		IPath fileLocation = stateLocation.append(CONFIGURATION_FILE);
+		return new File(fileLocation.toOSString());
 	}
 
 }
