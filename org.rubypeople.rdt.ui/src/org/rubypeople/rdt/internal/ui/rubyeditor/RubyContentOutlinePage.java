afa166dd54123919cd223ccbf2fa374d78ffecaa
cawilliams <cawilliams@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2004-03-13 18:48:24 +0000

    content outline page passes its text editor reference to the provider
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@327 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.ui/src/org/rubypeople/rdt/internal/ui/rubyeditor/RubyContentOutlinePage.java
+++ org.rubypeople.rdt.ui/src/org/rubypeople/rdt/internal/ui/rubyeditor/RubyContentOutlinePage.java
@@ -3,20 +3,24 @@ package org.rubypeople.rdt.internal.ui.rubyeditor;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.editors.text.TextEditor;
 import org.eclipse.ui.views.contentoutline.ContentOutlinePage;
 
 public class RubyContentOutlinePage extends ContentOutlinePage {
-	protected IDocument document;
 
-	public RubyContentOutlinePage(IDocument document) {
+	protected IDocument document;
+	private TextEditor fTextEditor;
+
+	public RubyContentOutlinePage(IDocument document, TextEditor textEditor) {
 		this.document = document;
+		this.fTextEditor = textEditor;
 	}
 
 	public void createControl(Composite parent) {
 		super.createControl(parent);
 
 		TreeViewer tree = getTreeViewer();
-		tree.setContentProvider(new RubyOutlineContentProvider());
+		tree.setContentProvider(new RubyOutlineContentProvider(fTextEditor));
 		tree.setLabelProvider(new RubyOutlineLabelProvider());
 		tree.setInput(document);
 	}
