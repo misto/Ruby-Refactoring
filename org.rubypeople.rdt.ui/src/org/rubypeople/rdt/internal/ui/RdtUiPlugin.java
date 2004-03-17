d06921532543422b4a55fada49fef105bac4a751
cawilliams <cawilliams@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2004-03-17 01:03:04 +0000

    sets defaults for annotations and text editor preferences
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@341 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.ui/src/org/rubypeople/rdt/internal/ui/RdtUiPlugin.java
+++ org.rubypeople.rdt.ui/src/org/rubypeople/rdt/internal/ui/RdtUiPlugin.java
@@ -10,7 +10,9 @@ import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.editors.text.TextEditorPreferenceConstants;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.eclipse.ui.texteditor.MarkerAnnotationPreferences;
 import org.rubypeople.rdt.internal.core.RubyPlugin;
 import org.rubypeople.rdt.internal.formatter.CodeFormatter;
 import org.rubypeople.rdt.internal.ui.text.RubyColorConstants;
@@ -19,26 +21,30 @@ import org.rubypeople.rdt.internal.ui.text.ruby.RubyProjectInformationProvider;
 import org.rubypeople.rdt.ui.PreferenceConstants;
 
 public class RdtUiPlugin extends AbstractUIPlugin implements RubyColorConstants {
+
 	protected static RdtUiPlugin plugin;
-	
+
 	public static final String PLUGIN_ID = "org.rubypeople.rdt.ui"; //$NON-NLS-1$
 	public static final String RUBY_RESOURCES_VIEW_ID = PLUGIN_ID + ".ViewRubyResources"; //$NON-NLS-1$
 	public static final String EDITOR_ID = "org.rubypeople.rdt.ui.EditorRubyFile"; //$NON-NLS-1$
-	public static final String EXTERNAL_FILES_EDITOR_ID = "org.rubypeople.rdt.ui.ExternalRubyEditor" ; //$NON-NLS-1$
-	
+	public static final String EXTERNAL_FILES_EDITOR_ID = "org.rubypeople.rdt.ui.ExternalRubyEditor"; //$NON-NLS-1$
 
-	protected RubyTextTools textTools ;
-	protected CodeFormatter codeFormatter ;
+	protected RubyTextTools textTools;
+	protected CodeFormatter codeFormatter;
 
 	public RdtUiPlugin(IPluginDescriptor descriptor) {
 		super(descriptor);
 		plugin = this;
-		// initialize textTools before any RubyEditor gets initialized, so that textTools can
-		// register for property change events first. If the registration takes place in the
-		// wrong order, changes to properties in the preferences page are not immediately updated
+		// initialize textTools before any RubyEditor gets initialized, so that
+		// textTools can
+		// register for property change events first. If the registration takes
+		// place in the
+		// wrong order, changes to properties in the preferences page are not
+		// immediately updated
 		// within the ruby editors.
 		textTools = new RubyTextTools();
-		// Call the method to force it to build the library model when the plugin is loaded
+		// Call the method to force it to build the library model when the
+		// plugin is loaded
 		RubyProjectInformationProvider.instance().getLibraryClassesAndModules();
 	}
 
@@ -49,7 +55,7 @@ public class RdtUiPlugin extends AbstractUIPlugin implements RubyColorConstants 
 	public static IWorkspace getWorkspace() {
 		return RubyPlugin.getWorkspace();
 	}
-	
+
 	public static IWorkbenchWindow getActiveWorkbenchWindow() {
 		return getDefault().getWorkbench().getActiveWorkbenchWindow();
 	}
@@ -61,33 +67,33 @@ public class RdtUiPlugin extends AbstractUIPlugin implements RubyColorConstants 
 	public static void log(Throwable e) {
 		log(new Status(IStatus.ERROR, PLUGIN_ID, IStatus.ERROR, RdtUiMessages.getString("RdtUiPlugin.internalErrorOccurred"), e)); //$NON-NLS-1$
 	}
-	
+
 	public static void log(int severity, String message, Throwable e) {
-		Status status = new Status(severity, PLUGIN_ID, IStatus.OK, message, e) ;
-		RdtUiPlugin.log(status) ;
+		Status status = new Status(severity, PLUGIN_ID, IStatus.OK, message, e);
+		RdtUiPlugin.log(status);
 	}
-	
+
 	public static Shell getActiveWorkbenchShell() {
 		return getActiveWorkbenchWindow().getShell();
 	}
-	
+
 	public RubyTextTools getTextTools() {
 		return textTools;
 	}
-	
+
 	public CodeFormatter getCodeFormatter() {
 		if (codeFormatter == null) {
-			codeFormatter = new CodeFormatter() ;
+			codeFormatter = new CodeFormatter();
 		}
-		codeFormatter.setIndentation(this.getPreferenceStore().getInt(PreferenceConstants.FORMAT_INDENTATION)) ;
-		return codeFormatter ;
+		codeFormatter.setIndentation(this.getPreferenceStore().getInt(PreferenceConstants.FORMAT_INDENTATION));
+		return codeFormatter;
 	}
-	
+
 	protected void initializeDefaultPreferences(IPreferenceStore store) {
 		PreferenceConverter.setDefault(store, RUBY_DEFAULT, new RGB(0, 0, 0));
 		store.setDefault(RUBY_DEFAULT + RUBY_ISBOLD_APPENDIX, false);
 		PreferenceConverter.setDefault(store, RUBY_KEYWORD, new RGB(164, 53, 122));
-		store.setDefault(RUBY_KEYWORD + RUBY_ISBOLD_APPENDIX, true);		
+		store.setDefault(RUBY_KEYWORD + RUBY_ISBOLD_APPENDIX, true);
 		PreferenceConverter.setDefault(store, RUBY_STRING, new RGB(15, 120, 142));
 		store.setDefault(RUBY_STRING + RUBY_ISBOLD_APPENDIX, false);
 		PreferenceConverter.setDefault(store, RUBY_MULTI_LINE_COMMENT, new RGB(247, 32, 64));
@@ -96,8 +102,10 @@ public class RdtUiPlugin extends AbstractUIPlugin implements RubyColorConstants 
 		store.setDefault(RUBY_SINGLE_LINE_COMMENT + RUBY_ISBOLD_APPENDIX, false);
 		//
 		PreferenceConverter.setDefault(store, RUBY_CONTENT_ASSISTANT_BACKGROUND, new RGB(150, 150, 0));
-		PreferenceConstants.initializeDefaultValues(store) ;
+		PreferenceConstants.initializeDefaultValues(store);
 		super.initializeDefaultPreferences(store);
+		MarkerAnnotationPreferences.initializeDefaultValues(store);
+		TextEditorPreferenceConstants.initializeDefaultValues(store);
 	}
 
 	/**
@@ -105,11 +113,11 @@ public class RdtUiPlugin extends AbstractUIPlugin implements RubyColorConstants 
 	 */
 	public static IWorkbenchPage getActivePage() {
 		IWorkbenchWindow window = getDefault().getWorkbench().getActiveWorkbenchWindow();
-		if(window == null)
+		if (window == null)
 			return null;
 		else
 			return getDefault().getWorkbench().getActiveWorkbenchWindow().getActivePage();
-		
+
 	}
 
 }
