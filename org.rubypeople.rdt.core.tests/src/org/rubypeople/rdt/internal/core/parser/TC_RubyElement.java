8114fe24c8e275b10c048c03ed64408458c46d84
cawilliams <cawilliams@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2004-03-20 18:12:32 +0000

    new test class for rubyElement
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@352 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

---
+++ org.rubypeople.rdt.core.tests/src/org/rubypeople/rdt/internal/core/parser/TC_RubyElement.java
@@ -0,0 +1,32 @@
+/*
+ * Created on Mar 20, 2004
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+package org.rubypeople.rdt.internal.core.parser;
+
+import junit.framework.TestCase;
+
+
+/**
+ * @author Chris
+ *
+ * To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+public class TC_RubyElement extends TestCase {
+
+	public void testGetElementsDoesntReturnLists() {
+		RubyElement element = new RubyElement("someElement", new Position(0,1));
+		RubyBlock block = new RubyBlock("block", new Position(1,1));
+		element.addElement(block);
+		RubyInstanceVariable var = new RubyInstanceVariable("var", new Position(1,1));
+		block.addElement(var);
+		Object[] elements = element.getElements();
+		assertEquals(var.getClass(), elements[0].getClass());
+		assertEquals(var, elements[0]);
+	}
+	
+	
+}
