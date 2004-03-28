2d34e08f2790bc534d904692018f3b9ac2fd1d4b
cawilliams <cawilliams@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2004-03-28 19:23:32 +0000

    formats entire script if no selection is made. Otherwise attempts to format selection (according to how it used to be implemented)
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@419 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.ui/src/org/rubypeople/rdt/ui/actions/FormatAction.java
+++ org.rubypeople.rdt.ui/src/org/rubypeople/rdt/ui/actions/FormatAction.java
@@ -2,7 +2,10 @@ package org.rubypeople.rdt.ui.actions;
 
 import java.util.ResourceBundle;
 
+import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.TextSelection;
+import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.texteditor.ITextEditor;
 import org.eclipse.ui.texteditor.TextEditorAction;
 import org.rubypeople.rdt.internal.ui.RdtUiPlugin;
@@ -15,20 +18,24 @@ public class FormatAction extends TextEditorAction {
 
 	public void run() {
 		IDocument doc = this.getTextEditor().getDocumentProvider().getDocument(this.getTextEditor().getEditorInput());
-	 	
-		           String allFormatted = RdtUiPlugin.getDefault().getCodeFormatter().formatString(doc.get());
-		doc.set(allFormatted);
-		/*try {
+
+		try {
 			ISelection selection = this.getTextEditor().getSelectionProvider().getSelection();
 			if (selection instanceof TextSelection) {
 				TextSelection textSelection = (TextSelection) selection;
-				String formatted = RdtUiPlugin.getDefault().getCodeFormatter().formatString(textSelection.getText());
-				doc.replace(textSelection.getOffset(), textSelection.getLength(), formatted);
+				String text = textSelection.getText();
+				if (text == null || text.length() == 0) {
+					String allFormatted = RdtUiPlugin.getDefault().getCodeFormatter().formatString(doc.get());
+					doc.set(allFormatted);
+				} else {
+					String formatted = RdtUiPlugin.getDefault().getCodeFormatter().formatString(text);
+					doc.replace(textSelection.getOffset(), textSelection.getLength(), formatted);
+				}
 			}
-			
+
 		} catch (BadLocationException e) {
 			RdtUiPlugin.log(e);
-		}*/
+		}
 
 		super.run();
 	}
