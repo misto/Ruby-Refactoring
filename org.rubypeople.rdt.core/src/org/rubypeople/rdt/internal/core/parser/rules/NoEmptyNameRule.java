dc5b7e6c1bdf8b3af1c00125692365ad5e256c19
cawilliams <cawilliams@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2004-04-06 01:09:01 +0000

    rule stipulating that the element must not have an empty name
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@455 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

---
+++ org.rubypeople.rdt.core/src/org/rubypeople/rdt/internal/core/parser/rules/NoEmptyNameRule.java
@@ -0,0 +1,72 @@
+/*
+ * Author: C.Williams
+ * 
+ * Copyright (c) 2004 RubyPeople.
+ * 
+ * This file is part of the Ruby Development Tools (RDT) plugin for eclipse.
+ * You can get copy of the GPL along with further information about RubyPeople
+ * and third party software bundled with RDT in the file
+ * org.rubypeople.rdt.core_0.4.0/RDT.license or otherwise at
+ * http://www.rubypeople.org/RDT.license.
+ * 
+ * RDT is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * RDT is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * RDT; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA 02111-1307 USA
+ */
+package org.rubypeople.rdt.internal.core.parser.rules;
+
+import org.rubypeople.rdt.internal.core.parser.ParseError;
+import org.rubypeople.rdt.internal.core.parser.ast.RubyElement;
+
+public class NoEmptyNameRule implements ParseRule {
+
+	private RubyElement element;
+
+	/**
+	 * @param token
+	 */
+	public NoEmptyNameRule(RubyElement token) {
+		this.element = token;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.rubypeople.rdt.internal.core.parser.ParseRule#isAllowed()
+	 */
+	public boolean isAllowed() {
+		if (element.isType(RubyElement.GLOBAL)) {
+			return element.getName().length() > 1;
+		}
+		if (element.isType(RubyElement.INSTANCE_VAR)) {
+			return element.getName().length() > 1;
+		}
+		if (element.isType(RubyElement.CLASS_VAR)) {
+			return element.getName().length() > 2;
+		}
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.rubypeople.rdt.internal.core.parser.ParseRule#getError()
+	 */
+	public ParseError getError() {
+		return new ParseError("Incomplete variable declaration", element.getStart().getLineNumber(), element.getStart().getOffset(), element.getStart().getOffset() + element.getName().length());
+	}
+
+	/* (non-Javadoc)
+	 * @see org.rubypeople.rdt.internal.core.parser.ParseRule#getSeverity()
+	 */
+	public int getSeverity() {
+		return ParseRule.ERROR;
+	}
+
+}
