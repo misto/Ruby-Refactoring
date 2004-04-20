1ac6a7275f1e679a52765d8c74db4d4ea0061893
cawilliams <cawilliams@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2004-04-20 22:22:16 +0000

    added license comment
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@497 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.core/src/org/rubypeople/rdt/internal/core/parser/ParseError.java
+++ org.rubypeople.rdt.core/src/org/rubypeople/rdt/internal/core/parser/ParseError.java
@@ -1,20 +1,32 @@
 /*
- * Created on Mar 13, 2004
+ * Author: C.Williams
  * 
- * To change the template for this generated file go to Window - Preferences -
- * Java - Code Generation - Code and Comments
+ * Copyright (c) 2004 RubyPeople.
+ * 
+ * This file is part of the Ruby Development Tools (RDT) plugin for eclipse. You
+ * can get copy of the GPL along with further information about RubyPeople and
+ * third party software bundled with RDT in the file
+ * org.rubypeople.rdt.core_0.4.0/RDT.license or otherwise at
+ * http://www.rubypeople.org/RDT.license.
+ * 
+ * RDT is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * RDT is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * RDT; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA 02111-1307 USA
  */
 package org.rubypeople.rdt.internal.core.parser;
 
 import org.eclipse.core.resources.IMarker;
 import org.rubypeople.rdt.internal.core.parser.ast.RubyElement;
 
-/**
- * @author Chris
- * 
- * To change the template for this generated type comment go to Window -
- * Preferences - Java - Code Generation - Code and Comments
- */
 public class ParseError {
 
 	private String error;
--- org.rubypeople.rdt.ui/src/org/rubypeople/rdt/internal/ui/resourcesview/NewWizardsActionGroup.java
+++ org.rubypeople.rdt.ui/src/org/rubypeople/rdt/internal/ui/resourcesview/NewWizardsActionGroup.java
@@ -1,21 +1,39 @@
 /*
- * Created on Mar 2, 2004
+ * Author: C.Williams
  * 
- * To change the template for this generated file go to Window - Preferences - Java - Code Generation - Code and Comments
+ * Copyright (c) 2004 RubyPeople.
+ * 
+ * This file is part of the Ruby Development Tools (RDT) plugin for eclipse. You
+ * can get copy of the GPL along with further information about RubyPeople and
+ * third party software bundled with RDT in the file
+ * org.rubypeople.rdt.core_0.4.0/RDT.license or otherwise at
+ * http://www.rubypeople.org/RDT.license.
+ * 
+ * RDT is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * RDT is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * RDT; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA 02111-1307 USA
  */
 package org.rubypeople.rdt.internal.ui.resourcesview;
 
+import org.eclipse.core.resources.IResource;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IWorkbenchPartSite;
 import org.eclipse.ui.actions.ActionGroup;
 import org.eclipse.ui.actions.NewWizardMenu;
+import org.rubypeople.rdt.internal.core.parser.ast.IRubyElement;
 
-/**
- * @author Chris
- * 
- * To change the template for this generated type comment go to Window - Preferences - Java - Code Generation - Code and Comments
- */
 public class NewWizardsActionGroup extends ActionGroup {
 
 	private IWorkbenchPartSite fSite;
@@ -33,29 +51,26 @@ public class NewWizardsActionGroup extends ActionGroup {
 	public void fillContextMenu(IMenuManager menu) {
 		super.fillContextMenu(menu);
 
-		// TODO Find out why the context is null. Code is commented out until we can fix that. Should check to see if we're right-clicking on an IRubyElement
-		//		ISelection selection = getContext().getSelection();
-		//		if (selection instanceof IStructuredSelection) {
-		//			IStructuredSelection sel = (IStructuredSelection) selection;
-		//			if (sel.size() <= 1 && isNewTarget(sel.getFirstElement())) {
-		IMenuManager newMenu = new MenuManager("New");
-		menu.appendToGroup("group.new", newMenu);
-		new NewWizardMenu(newMenu, fSite.getWorkbenchWindow(), false);
-		//			}
-		//		}
+		// TODO Find out why the context is null. Code is commented out until we
+		// can fix that. Should check to see if we're right-clicking on an
+		// IRubyElement
+		ISelection selection = getContext().getSelection();
+		if (selection instanceof IStructuredSelection) {
+			IStructuredSelection sel = (IStructuredSelection) selection;
+			if (sel.size() <= 1 && isNewTarget(sel.getFirstElement())) {
+				IMenuManager newMenu = new MenuManager("New");
+				menu.appendToGroup("group.new", newMenu);
+				new NewWizardMenu(newMenu, fSite.getWorkbenchWindow(), false);
+			}
+		}
 
 	}
 
-	//	private boolean isNewTarget(Object element) {
-	//		if (element == null)
-	//			return true;
-	//		if (element instanceof IResource) {
-	//			return true;
-	//		}
-	//		if (element instanceof IRubyElement) {
-	//			return true;
-	//		}
-	//		return false;
-	//	}
+	private boolean isNewTarget(Object element) {
+		if (element == null) return true;
+		if (element instanceof IResource) { return true; }
+		if (element instanceof IRubyElement) { return true; }
+		return false;
+	}
 
 }
