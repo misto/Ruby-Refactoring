1bb359bbb9aa7b647514e9587f69936a23e827b4
mbarchfe <mbarchfe@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2004-05-03 17:24:44 +0000

    uses RubyResourcesView to check if enabled
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@507 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.ui/src/org/rubypeople/rdt/internal/ui/RubyViewerFilter.java
+++ org.rubypeople.rdt.ui/src/org/rubypeople/rdt/internal/ui/RubyViewerFilter.java
@@ -1,3 +1,13 @@
+/*
+ * Author: Adam Williams, Markus Barchfeld
+ * 
+ * Copyright (c) 2004 RubyPeople.
+ * 
+ * This file is part of the Ruby Development Tools (RDT) plugin for eclipse. RDT
+ * is subject to the "Common Public License (CPL) v 1.0". You may not use RDT
+ * except in compliance with the License. For further information see
+ * org.rubypeople.rdt/rdt.license.
+ */
 package org.rubypeople.rdt.internal.ui;
 
 import org.eclipse.core.resources.IFolder;
@@ -5,22 +15,23 @@ import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerFilter;
 import org.rubypeople.rdt.core.RubyElement;
+import org.rubypeople.rdt.internal.ui.resourcesview.RubyResourcesView;
 
 public class RubyViewerFilter extends ViewerFilter {
 
-	public RubyViewerFilter() {
-		super();
+	private RubyResourcesView rubyResourcesView;
+
+	public RubyViewerFilter(RubyResourcesView rubyResourcesView) {
+		this.rubyResourcesView = rubyResourcesView;
 	}
 
 	public boolean select(Viewer viewer, Object parentElement, Object element) {
-		if (element instanceof IFolder)
-			return true;
+		if (!rubyResourcesView.isRubyFilesOnlyFilterActivated()) { return true; }
+		if (element instanceof IFolder) { return true; }
 
 		IAdaptable adaptable = (IAdaptable) element;
 		RubyElement rubyElement = (RubyElement) adaptable.getAdapter(RubyElement.class);
-		if (rubyElement != null)
-			return true;
-
-		return false;
+		return rubyElement != null;
 	}
+
 }
