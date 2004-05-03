8afee950347b20c633d7f699d05265672a27159e
mbarchfe <mbarchfe@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2004-05-03 21:03:11 +0000

    fixed null pointer exception when editor is RubyExternal
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@513 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.ui/src/org/rubypeople/rdt/internal/ui/resourcesview/RubyResourcesView.java
+++ org.rubypeople.rdt.ui/src/org/rubypeople/rdt/internal/ui/resourcesview/RubyResourcesView.java
@@ -11,7 +11,11 @@
 
 package org.rubypeople.rdt.internal.ui.resourcesview;
 
+import org.eclipse.core.resources.IFile;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IFileEditorInput;
 import org.eclipse.ui.IMemento;
 import org.eclipse.ui.IViewSite;
 import org.eclipse.ui.PartInitException;
@@ -61,5 +65,22 @@ public class RubyResourcesView extends ResourceNavigator {
 		if (memento != null) {
 			this.restoreRubyFilesOnlyFilterActivated(memento);
 		}
+	}
+	protected void editorActivated(IEditorPart editor) {
+		if (!isLinkingEnabled()) {
+			return;
+		}
+
+		IEditorInput input = editor.getEditorInput();
+		if (input instanceof IFileEditorInput) {
+			IFileEditorInput fileInput = (IFileEditorInput) input;
+			IFile file = fileInput.getFile();
+			// Quick Fix: if Editor is RubyExternal, then file is null
+			// TODO: Check if RubyExternalEditor's input should be IFileEditorInput at all
+			if (file == null) {
+				return ;
+			}			
+		}
+		super.editorActivated(editor) ;
 	}
 }
