a803e4ebf431f1e197510ded2c697a506aec1563
mbarchfe <mbarchfe@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2004-05-03 17:17:58 +0000

    added console tracker test case
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@503 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.debug.ui.tests/src/org/rubypeople/rdt/internal/debug/ui/tests/launcher/TS_Launcher.java
+++ org.rubypeople.rdt.debug.ui.tests/src/org/rubypeople/rdt/internal/debug/ui/tests/launcher/TS_Launcher.java
@@ -3,6 +3,9 @@ package org.rubypeople.rdt.internal.debug.ui.tests.launcher;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+import org.rubypeople.rdt.internal.debug.ui.tests.TC_RubyConsoleTracker;
+import org.rubypeople.rdt.internal.debug.ui.tests.TC_RubySourceLocator;
+
 public class TS_Launcher {
 
 	public static Test suite() {
@@ -13,6 +16,7 @@ public class TS_Launcher {
 		suite.addTest(new TestSuite(TC_RubyEntryPointTab.class));
 		suite.addTest(new TestSuite(TC_RubyEnvironmentTab.class));
 		suite.addTest(new TestSuite(TC_RubySourceLocator.class));
+		suite.addTest(new TestSuite(TC_RubyConsoleTracker.class));
 		//$JUnit-END$
 		return suite;
 	}
