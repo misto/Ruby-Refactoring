7b65d4e57320ce504f745eb1592f6e1b5a75b2b4
mbarchfe <mbarchfe@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2004-05-08 15:38:25 +0000

    added CREATE_PARSER_ANNOTATIONS
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@535 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.ui/src/org/rubypeople/rdt/ui/PreferenceConstants.java
+++ org.rubypeople.rdt.ui/src/org/rubypeople/rdt/ui/PreferenceConstants.java
@@ -11,10 +11,13 @@ public class PreferenceConstants {
 	
 	public static final String FORMAT_INDENTATION = "formatIndentation"; //$NON-NLS-1$
 	public static final String FORMAT_USE_TAB = "formatUseTab"; //$NON-NLS-1$	
+	public static final String CREATE_PARSER_ANNOTATIONS = "createParserAnnotations"; //$NON-NLS-1$
+	
 	
 	public static void initializeDefaultValues(IPreferenceStore store) {
 		store.setDefault(PreferenceConstants.FORMAT_INDENTATION, 2);
 		store.setDefault(PreferenceConstants.FORMAT_USE_TAB, false);
+		store.setDefault(PreferenceConstants.CREATE_PARSER_ANNOTATIONS, false);
 	}	
 	
 }
