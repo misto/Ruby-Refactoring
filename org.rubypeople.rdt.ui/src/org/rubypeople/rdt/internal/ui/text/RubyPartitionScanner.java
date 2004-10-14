f6bc59a179afbc8ea1a7f10bae521a48a72219c1
cawilliams <cawilliams@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2004-10-14 02:48:33 +0000

    add single line comment partitioning rule
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@635 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.ui/src/org/rubypeople/rdt/internal/ui/text/RubyPartitionScanner.java
+++ org.rubypeople.rdt.ui/src/org/rubypeople/rdt/internal/ui/text/RubyPartitionScanner.java
@@ -3,6 +3,7 @@ package org.rubypeople.rdt.internal.ui.text;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.jface.text.rules.EndOfLineRule;
 import org.eclipse.jface.text.rules.IPredicateRule;
 import org.eclipse.jface.text.rules.IToken;
 import org.eclipse.jface.text.rules.MultiLineRule;
@@ -15,6 +16,7 @@ public class RubyPartitionScanner extends RuleBasedPartitionScanner {
 	public final static String SKIP = "partition_scanner_ruby_skip";
 	public final static String STRING = "partition_scanner_ruby_string";
 	public final static String MULTI_LINE_COMMENT = "partition_scanner_ruby_multiline_comment";
+	public static final String SINGLE_LINE_COMMENT = "partition_scanner_ruby_singleline_comment";
 
 	public RubyPartitionScanner() {
 		super();
@@ -25,6 +27,7 @@ public class RubyPartitionScanner extends RuleBasedPartitionScanner {
 		IToken skip = new Token(SKIP);
 		IToken string = new Token(STRING);
 		IToken multiLineComment = new Token(MULTI_LINE_COMMENT);
+		IToken singleLineComment = new Token(SINGLE_LINE_COMMENT);
 
 		List rules = new ArrayList();
 
@@ -32,6 +35,10 @@ public class RubyPartitionScanner extends RuleBasedPartitionScanner {
 		// strings (and therefore have quotes inside them which don't end the
 		// partition)
 		rules.add(new SingleLineRule("\"", "\"", string, '\\'));
+		
+		// Add rule for single line comments.
+		rules.add(new EndOfLineRule("#", singleLineComment));
+		
 		rules.add(new SingleLineRule("'", "'", skip, '\\'));
 		rules.add(new MultiLineRule("=begin", "=end", multiLineComment));
 
