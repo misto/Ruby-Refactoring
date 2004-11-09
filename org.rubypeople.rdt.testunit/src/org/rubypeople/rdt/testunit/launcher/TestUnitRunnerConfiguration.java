a793ae6f9cb06353c4c123489f5cbdd05b8c6de0
cawilliams <cawilliams@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2004-11-09 01:13:33 +0000

    remove unused argument, default to not keeping the runner alive
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@659 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.testunit/src/org/rubypeople/rdt/testunit/launcher/TestUnitRunnerConfiguration.java
+++ org.rubypeople.rdt.testunit/src/org/rubypeople/rdt/testunit/launcher/TestUnitRunnerConfiguration.java
@@ -1,6 +1,5 @@
 package org.rubypeople.rdt.testunit.launcher;
 
-import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.debug.core.ILaunchConfiguration;
@@ -14,39 +13,38 @@ public class TestUnitRunnerConfiguration extends InterpreterRunnerConfiguration 
 	}
 
 	public String getAbsoluteFileName() {
-		IProject project = getProject().getProject();
-	
 		String filename = getFileName();
 		if (Platform.getOS().equals(Platform.OS_WIN32)) {
 			// just searching for a colon without consideration of the Platform
 			// could chop off linux filenames which may contain colons
-			if (filename.indexOf(':') != -1) { 
-				filename = filename.substring(filename.indexOf(':') + 1); 
+			if (filename.indexOf(':') != -1) {
+				filename = filename.substring(filename.indexOf(':') + 1);
 			}
-		}		
-		return filename ;
+		}
+		return filename;
 	}
-	
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.rubypeople.rdt.internal.launching.InterpreterRunnerConfiguration#getProgramArguments()
 	 */
 	public String getProgramArguments() {
 		int port = 6789;
 		String fileName = "";
 		String testClass = "";
-		boolean keepAlive = true;
+		boolean keepAlive = false;
 		try {
 			// Pull out the port and other unit testing variables
 			// and convert them into command line args
 			port = configuration.getAttribute(TestUnitLaunchConfiguration.PORT_ATTR, 6789);
 			fileName = configuration.getAttribute(TestUnitLaunchConfiguration.LAUNCH_CONTAINER_ATTR, "");
-			keepAlive = configuration.getAttribute(TestUnitLaunchConfiguration.ATTR_KEEPRUNNING, true);
+			keepAlive = configuration.getAttribute(TestUnitLaunchConfiguration.ATTR_KEEPRUNNING, false);
 			testClass = configuration.getAttribute(TestUnitLaunchConfiguration.TESTTYPE_ATTR, "");
 		} catch (CoreException e) {
 			TestunitPlugin.log(e);
 		}
-		
+
 		return fileName + " " + port + " " + keepAlive + " " + testClass;
 	}
 }
