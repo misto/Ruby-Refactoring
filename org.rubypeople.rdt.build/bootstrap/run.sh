63fa3d2139a5daa6dd963c2069829afdfac09131
mbarchfe <mbarchfe@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2004-11-09 20:06:28 +0000

    *** empty log message ***
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@661 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.build/bootstrap/run.sh
+++ org.rubypeople.rdt.build/bootstrap/run.sh
@@ -1,35 +1,37 @@
 #!/bin/sh
-#REM The eclipse directory from which eclipse will be started for building and testing.
-#REM It is run with the AntRunner
-eclipseDir=/home/markus/eclipse-3.0-gtk
 
-#REM The version of the PDE Build plug-in is needed to determine the script directory
-#REM The version will probably be equal to the eclipse version
-pdeBuildPluginVersion=3.0.0
+#set eclipseDir=D:\eclipse-3.0
+#set pdeBuildPluginVersion=3.0.0
+#set pluginAutomaticBuildDir=D:\java\rdt-workspace-ssh\org.rubypeople.rdt.build\bootstrap
+#set buildDirectory=D:\Temp\RdtBuildDir
+#set verboseAnt=-verbose
+#REM set verboseAnt=
+#set eclipseAutomatedTestHome=D:\eclipse-testing
+#set rubyInterpreter=D:\Program Files\ruby-1.8.2\bin\ruby.exe
+#set docbook.root=D:\java\docbook
+#set vm=java
 
-#REM The pluginAutomaticBuildDir contains the necessary files provided by the plug-in
-#REM itself for the build-process, e.g. build.properties, customTargets.xml.
-#REM It is not bound to reside in the workspace of the eclipse and can reside on any
-#REM accessible location
-#REM This is also the location of the workspace: a subdirectory workspace will be
-# createad in $pluginAutomaticBuildDir
-pluginAutomaticBuildDir=/laptop/java/rdt-workspace/org.rubypeople.rdt.build
-
-#REM 
-buildTargetDirectory=/tmp/PlugInBuildDir
-
-#REM default java executable
-vm=java
-
-#REM reset list of ant targets in test.xml to execute
 #REM set tests=
-#REM default switch to determine if eclipse should be reinstalled between running of tests
+#set installmode=clean
+
+#set os=win32
+#set ws=win32
+#set arch=x86
+
+#set ANT_CMD_LINE_ARGS=
+
+
+
+eclipseDir=/home/markus/eclipse-3.0.1
+pdeBuildPluginVersion=3.0.1
+pluginAutomaticBuildDir=/tmp/org.rubypeople.rdt.build/bootstrap
+buildDirectory=/tmp/PlugInBuildDir
+verboseAnt=-verbose
+eclipseAutomatedTestHome=/home/markus/eclipse-testing
+rubyInterpreter=/usr/local/bin/ruby
+docbookRoot=/home/markus/docbook
+vm=java
 installmode=clean
-
-#REM property file to pass to Ant scripts
-
-#REM default values for os, ws and arch
-# Must correspond to the builder-eclipse instance
 os=linux
 ws=gtk
 arch=x86
@@ -39,33 +41,18 @@ ANT_CMD_LINE_ARGS=
 
 
 
-#:processcmdlineargs
-
-#REM ****************************************************************
-#REM
-#REM Process command line arguments
-#REM
-#REM ****************************************************************
-
- 
-#if x%1==x goto run
-#if x%1==x-ws set ws=%2 && shift && shift && goto processcmdlineargs
-#if x%1==x-os set os =%2 && shift && shift && goto processcmdlineargs
-#if x%1==x-arch set arch=%2 && shift && shift && goto processcmdlineargs
-#if x%1==x-noclean set installmode=noclean && shift && goto processcmdlineargs
-#if x%1==x-properties set properties=-propertyfile %2 && shift && shift && goto processcmdlineargs
-#if x%1==x-vm set vm=%2 && shift && shift && goto processcmdlineargs
 
 
 
-#:run
-##REM ***************************************************************************
-##REM	Run tests by running Ant in Eclipse on the test.xml script
-##REM ***************************************************************************
+#REM %vm% -cp %eclipseDir%\startup.jar -Dosgi.ws=%ws% -Dosgi.os=%os% -Dosgi.arch=%arch% org.eclipse.core.launcher.Main 
+#-application org.eclipse.ant.core.antRunner -buildfile %buildfile% 
+#-data %buildDirectory%/workspace-build -Dconfigs="*,*,*" -Dbaseos=win32 -Dbasews=win32 -Dbasearch=x86 -Dbuilder=%pluginAutomaticBuildDir%  
+#"-D%installmode%=true" -DjavacFailOnError=true -DbuildDirectory=%buildDirectory% -DbaseLocation=%eclipseDir% %verboseAnt% 
+#-DeclipseAutomatedTestHome=%eclipseAutomatedTestHome% -Drdt.rubyInterpreter="%rubyInterpreter%" 
+#-Drdt-tests-workspace=%buildDirectory%/workspace-rdt-tests -Ddocbook.root="%docbook.root%"
+
 buildfile=$eclipseDir/plugins/org.eclipse.pde.build_$pdeBuildPluginVersion/scripts/build.xml
 echo Starting eclipse in $eclipseDir, $vm
-$vm -cp $eclipseDir/startup.jar org.eclipse.core.launcher.Main -application org.eclipse.ant.core.antRunner -buildfile $buildfile -data /tmp/workspace -Dbasews=$ws -Dbaseos=$os -Dbasearch=$arch -Dbuilder=$buildfile  -D$installmode=true -DbuildTargetDirectory=$buildTargetDirectory -verbose  -Dbuilder=$pluginAutomaticBuildDir -DbuildDirectory=/tmp/eclipseBuild -DbaseLocation=$eclipseDir
+cmd="$vm -cp $eclipseDir/startup.jar org.eclipse.core.launcher.Main -application org.eclipse.ant.core.antRunner -buildfile $buildfile -data $buildDirectory/workspace -Dbasews=$ws -Dbaseos=$os -Dbasearch=$arch -Dbuilder=$pluginAutomaticBuildDir  -D$installmode=true -DjavacFailOnError=true -DbuildDirectory=/tmp/eclipseBuild -DbaseLocation=$eclipseDir $verboseAnt -DeclipseAutomatedTestHome=$eclipseAutomatedTestHome -Drdt.rubyInterpreter="$rubyInterpreter" -Drdt-tests-workspace=$buildDirectory/workspace-rdt-tests -Ddocbook.root=$docbookRoot"
 echo $cmd
 `$cmd`
-
-#:end
