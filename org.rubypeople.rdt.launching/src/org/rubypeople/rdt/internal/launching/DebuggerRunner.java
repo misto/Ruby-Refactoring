63ba72bea8a6d004a22fdc4e078311093107368f
mbarchfe <mbarchfe@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2004-11-14 13:02:19 +0000

    changed behaviour when no connection can be made
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@679 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.launching/src/org/rubypeople/rdt/internal/launching/DebuggerRunner.java
+++ org.rubypeople.rdt.launching/src/org/rubypeople/rdt/internal/launching/DebuggerRunner.java
@@ -2,8 +2,11 @@ package org.rubypeople.rdt.internal.launching;
 
 import java.io.File;
 
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.model.IProcess;
+import org.rubypeople.rdt.internal.debug.core.DebuggerNotFoundException;
 import org.rubypeople.rdt.internal.debug.core.RdtDebugCorePlugin;
 import org.rubypeople.rdt.internal.debug.core.RubyDebuggerProxy;
 import org.rubypeople.rdt.internal.debug.core.model.RubyDebugTarget;
@@ -13,8 +16,15 @@ public class DebuggerRunner extends InterpreterRunner {
 	public IProcess run(InterpreterRunnerConfiguration configuration, ILaunch launch) {
 		IProcess process = super.run(configuration, launch);
 		RubyDebugTarget debugTarget = new RubyDebugTarget(launch, process);
-		new RubyDebuggerProxy(debugTarget).start();
-		launch.addDebugTarget(debugTarget);
+		RubyDebuggerProxy proxy = new RubyDebuggerProxy(debugTarget) ;
+		if (proxy.checkConnection()) {
+			proxy.start();
+			launch.addDebugTarget(debugTarget);	
+		}
+		else {
+			RdtLaunchingPlugin.log(new Status(IStatus.ERROR, RdtLaunchingPlugin.PLUGIN_ID, IStatus.ERROR, RdtLaunchingMessages.getString("RdtLaunchingPlugin.processTerminatedBecauseNoDebuggerConnection"),null)) ;
+			debugTarget.terminate();
+		}
 		return process;
 	}
 
