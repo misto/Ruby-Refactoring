35271ddc05b2cdcd8748ec56a1452b79149cba31
cawilliams <cawilliams@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2004-11-19 04:31:25 +0000

    puts most views and shortcuts into plugin.xml file. Add placeholder for problems view. fix reference to Outline view (use constant)
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@692 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.ui/src/org/rubypeople/rdt/internal/ui/RdtPerspectiveFactory.java
+++ org.rubypeople.rdt.ui/src/org/rubypeople/rdt/internal/ui/RdtPerspectiveFactory.java
@@ -1,4 +1,3 @@
-
 package org.rubypeople.rdt.internal.ui;
 
 import org.eclipse.debug.ui.IDebugUIConstants;
@@ -15,29 +14,20 @@ public class RdtPerspectiveFactory implements IPerspectiveFactory {
 	}
 
 	public void createInitialLayout(IPageLayout layout) {
-		layout.addNewWizardShortcut("org.rubypeople.rdt.ui.wizards.NewWizardProjectCreation");
-		layout.addNewWizardShortcut("org.eclipse.ui.wizards.new.folder");
-		layout.addNewWizardShortcut("org.eclipse.ui.wizards.new.file");
-		
- 		String existingEditorArea = layout.getEditorArea();
-		
-		IFolderLayout rubyResourcesArea = layout.createFolder("rubyResourcesArea", IPageLayout.LEFT, (float)0.26, existingEditorArea);
+		String existingEditorArea = layout.getEditorArea();
+
+		IFolderLayout rubyResourcesArea = layout.createFolder("rubyResourcesArea", IPageLayout.LEFT, (float) 0.26, existingEditorArea);
 		rubyResourcesArea.addView(RdtUiPlugin.RUBY_RESOURCES_VIEW_ID);
-		
-		IFolderLayout consoleArea = layout.createFolder("consoleArea", IPageLayout.BOTTOM, (float)0.75, existingEditorArea);
+
+		IFolderLayout consoleArea = layout.createFolder("consoleArea", IPageLayout.BOTTOM, (float) 0.75, existingEditorArea);
 		consoleArea.addView(IConsoleConstants.ID_CONSOLE_VIEW);
-		
+		consoleArea.addPlaceholder(IPageLayout.ID_PROBLEM_VIEW);
+
 		layout.addActionSet(IDebugUIConstants.LAUNCH_ACTION_SET);
 		layout.addActionSet(RubyActionSetIds.RUBY_ACTION_SET_ID);
-		
-		layout.addShowViewShortcut(IConsoleConstants.ID_CONSOLE_VIEW);
-		layout.addShowViewShortcut("org.eclipse.ui.views.ContentOutline");
-		layout.addShowViewShortcut(RdtUiPlugin.RUBY_RESOURCES_VIEW_ID);
-		
-		IFolderLayout bottomLeft = layout.createFolder("bottomLeft", 4, 0.5F, "rubyResourcesArea");
-		bottomLeft.addView("org.eclipse.ui.views.ContentOutline");
+
+		IFolderLayout bottomLeft = layout.createFolder("bottomLeft", IPageLayout.BOTTOM, (float) 0.5, "rubyResourcesArea");
+		bottomLeft.addView(IPageLayout.ID_OUTLINE);
 	}
-	
-	
 
 }
