f426eb24b278398927c49c0a64a8d1c74a68d10f
mbarchfe <mbarchfe@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2004-11-21 20:22:23 +0000

    cheat sheet added
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@701 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

---
+++ org.rubypeople.rdt.debug.ui/src/org/rubypeople/rdt/internal/debug/ui/cheatsheets/webservice/CopyContentAction.java
@@ -0,0 +1,45 @@
+package org.rubypeople.rdt.internal.debug.ui.cheatsheets.webservice;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jface.action.Action;
+import org.eclipse.ui.cheatsheets.ICheatSheetAction;
+import org.eclipse.ui.cheatsheets.ICheatSheetManager;
+import org.rubypeople.rdt.internal.core.RubyPlugin;
+import org.rubypeople.rdt.internal.ui.RdtUiPlugin;
+
+
+public class CopyContentAction extends Action implements ICheatSheetAction {
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.cheatsheets.ICheatSheetAction#run(java.lang.String[], org.eclipse.ui.cheatsheets.ICheatSheetManager)
+	 */
+	public void run(String[] params, ICheatSheetManager manager) {
+
+		
+		File sourceFile = new File(RubyPlugin.getOSDirectory(RdtUiPlugin.getDefault()) + params[0]);
+		if (!sourceFile.exists()) {
+		    this.notifyResult(false) ;
+		    return ;
+		}
+		
+		IFile dest = RdtUiPlugin.getWorkspace().getRoot().getFile(new Path(params[1])) ;
+		if (dest == null || !dest.exists()) {
+		    this.notifyResult(false) ;
+		    return ;
+		}
+		try {
+            InputStream inputStream = new FileInputStream(sourceFile) ;
+            dest.setContents(inputStream, true, true, null) ;
+            this.notifyResult(true) ;
+        } catch (Exception e) {
+		    this.notifyResult(false) ;
+        }
+
+	}
+
+}
---
+++ org.rubypeople.rdt.debug.ui/src/org/rubypeople/rdt/internal/debug/ui/cheatsheets/webservice/OpenNewRubyProjectWizardAction.java
@@ -0,0 +1,48 @@
+package org.rubypeople.rdt.internal.debug.ui.cheatsheets.webservice;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.ui.IActionDelegate;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.cheatsheets.ICheatSheetAction;
+import org.eclipse.ui.cheatsheets.ICheatSheetManager;
+import org.rubypeople.rdt.internal.ui.RdtUiPlugin;
+import org.rubypeople.rdt.internal.ui.wizards.NewProjectCreationWizard;
+
+/**
+ * @author markus
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class OpenNewRubyProjectWizardAction extends Action implements ICheatSheetAction  {
+	public OpenNewRubyProjectWizardAction() {
+		super("OpenProject"); //$NON-NLS-1$
+	}
+	
+	/**
+	 * @see IActionDelegate#run(IAction)
+	 */
+	public void run() {
+		run(new String [] {}, null);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.cheatsheets.ICheatSheetAction#run(java.lang.String[], org.eclipse.ui.cheatsheets.ICheatSheetManager)
+	 */
+	public void run(String[] params, ICheatSheetManager manager) {
+		NewProjectCreationWizard wizard = new NewProjectCreationWizard();
+		if (params.length > 0) {
+			wizard.setDefaultProjectName(params[0]) ;
+		}
+		wizard.init(PlatformUI.getWorkbench(), new StructuredSelection());
+		WizardDialog dialog = new WizardDialog(RdtUiPlugin.getActiveWorkbenchShell(), wizard);
+		dialog.create();
+		//SWTUtil.setDialogSize(dialog, 500, 500);
+		dialog.getShell().setText(wizard.getWindowTitle());
+		int result = dialog.open();
+		notifyResult(result==WizardDialog.OK);
+	}
+}
