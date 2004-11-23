a0c4cad09da48e9b40698f59a7b7adc2d80bb311
cawilliams <cawilliams@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2004-11-23 02:22:59 +0000

    point to RDTDebugUiPlugin, not RdtUiPlugin
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@705 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.debug.ui/src/org/rubypeople/rdt/internal/debug/ui/cheatsheets/webservice/CopyContentAction.java
+++ org.rubypeople.rdt.debug.ui/src/org/rubypeople/rdt/internal/debug/ui/cheatsheets/webservice/CopyContentAction.java
@@ -10,6 +10,7 @@ import org.eclipse.jface.action.Action;
 import org.eclipse.ui.cheatsheets.ICheatSheetAction;
 import org.eclipse.ui.cheatsheets.ICheatSheetManager;
 import org.rubypeople.rdt.internal.core.RubyPlugin;
+import org.rubypeople.rdt.internal.debug.ui.RdtDebugUiPlugin;
 import org.rubypeople.rdt.internal.ui.RdtUiPlugin;
 
 
@@ -18,28 +19,25 @@ public class CopyContentAction extends Action implements ICheatSheetAction {
 	/* (non-Javadoc)
 	 * @see org.eclipse.ui.cheatsheets.ICheatSheetAction#run(java.lang.String[], org.eclipse.ui.cheatsheets.ICheatSheetManager)
 	 */
-	public void run(String[] params, ICheatSheetManager manager) {
-
-		
-		File sourceFile = new File(RubyPlugin.getOSDirectory(RdtUiPlugin.getDefault()) + params[0]);
+	public void run(String[] params, ICheatSheetManager manager) {		
+		File sourceFile = new File(RubyPlugin.getOSDirectory(RdtDebugUiPlugin.getDefault()) + params[0]);
 		if (!sourceFile.exists()) {
-		    this.notifyResult(false) ;
-		    return ;
+		    this.notifyResult(false);
+		    return;
 		}
 		
-		IFile dest = RdtUiPlugin.getWorkspace().getRoot().getFile(new Path(params[1])) ;
+		IFile dest = RdtUiPlugin.getWorkspace().getRoot().getFile(new Path(params[1]));
 		if (dest == null || !dest.exists()) {
-		    this.notifyResult(false) ;
-		    return ;
+		    this.notifyResult(false);
+		    return;
 		}
 		try {
-            InputStream inputStream = new FileInputStream(sourceFile) ;
-            dest.setContents(inputStream, true, true, null) ;
-            this.notifyResult(true) ;
+            InputStream inputStream = new FileInputStream(sourceFile);
+            dest.setContents(inputStream, true, true, null);
+            this.notifyResult(true);
         } catch (Exception e) {
-		    this.notifyResult(false) ;
+		    this.notifyResult(false);
         }
-
 	}
 
 }
