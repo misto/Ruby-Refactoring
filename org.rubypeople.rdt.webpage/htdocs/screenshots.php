5cd69788b5fcfc8fdff72abfe69f981d6fb0ce69
mbarchfe <mbarchfe@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2004-12-19 23:07:38 +0000

    moved content from html files to php files
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@755 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

---
+++ org.rubypeople.rdt.webpage/htdocs/screenshots.php
@@ -0,0 +1,34 @@
+<table bgcolor="#ffffff" border="0" cellpadding="2" cellspacing="5" width="100%">
+  <tr>
+    <td colspan="2" align="left" bgcolor="#CC0000" valign="top"><b><font color="#ffffff" face="Arial,Helvetica">Screenshots</font></b></td>
+  </tr>
+</table>
+
+<p align="left">
+  <img src="images/arrow.gif" alt="->" border="0" height="16" width="16">
+  <font face="arial,helvetica,geneva" size="-1"><b>Ruby Outline View</b>
+</p>
+<p align="center">
+  <img src="images/screenshots/outline.jpg">
+</p>
+<p align="left">
+  <img src="images/arrow.gif" alt="->" border="0" height="16" width="16">
+  <font face="arial,helvetica,geneva" size="-1"><b>Test::Unit Runner/View</b>
+</p>
+<p align="center">
+  <img src="images/screenshots/testunit.png">
+</p>
+<p align="left">
+  <img src="images/arrow.gif" alt="->" border="0" height="16" width="16">
+  <font face="arial,helvetica,geneva" size="-1"><b>Regular Expression Tester</b>
+</p>
+<p align="center">
+  <img src="images/screenshots/regexp.png">
+</p>
+<p align="left">
+  <img src="images/arrow.gif" alt="->" border="0" height="16" width="16">
+  <font face="arial,helvetica,geneva" size="-1"><b>Ruby Editor/Perspective</b>
+</p>
+<p align="center">
+  <img src="images/screenshots/editor.jpg">
+</p>
---
+++ org.rubypeople.rdt.webpage/htdocs/welcome.php
@@ -0,0 +1,64 @@
+
+  <table width="100%">
+    <tr>
+      <td align="left"><font face="arial,helvetica,geneva"><b>Ruby Development Tools</b></font></td>
+      <td align="right"><img src="images/rdt_big.png"></td>
+    </tr>
+  </table>
+  
+  <table bgcolor="#ffffff" border="0" cellpadding="2" cellspacing="5" width="100%">
+    <tr>
+      <td align="right" valign="top" width="20"><img src="images/arrow.gif" alt="" border="0" height="16" width="16"></td>
+      <td valign="top"><font face="arial,helvetica,geneva" size="-1"><b> Welcome to RDT</b>
+        <br/>
+        <p>RDT is an open source Ruby IDE for the Eclipse platform.<br>
+Features supported are syntax highlighting, on the fly syntax check,
+graphical outline, Test::Unit view/runner, Ruby application launching, 
+content assist, source formatter, and a Ruby debugger.<br/>
+A regular expression plugin is also available (Thanks to the <a href="http://e-p-i-c.sourceforge.net/" target="_top">EPIC</a> project).<br/>
+        </p>
+        <br/>
+        </font>
+      </td>
+    </tr>
+	</table>
+
+		<div id="mainPartHeader">
+			<a name="WhatsNew">What's New</a>
+		 </div>
+
+
+    <!-- INSERT NEW NEWS HERE!!! -->
+	<table>
+    <tr>
+      <td align="right" valign="top" width="20"><img src="images/arrow.gif" alt="->" border="0" height="16" width="16"></td>
+      <td valign="top"><font face="arial,helvetica,geneva" size="-1"><b> RDT 0.5.0 Released (2004-11-28)</b>
+        <br/>
+<p>This release includes a significant number of new features for RDT. New 
+features included in this release: Test::Unit runner, Template code completions 
+(which can be created and edited by users), a Regular expression plugin, an 
+introductory tutorial/cheat sheet, integration into Eclipse's Welcome screens, 
+and assorted bugfixes.  
+<br/>
+Please note that the 0.5.0 release is not yet available on the update site, but will be 
+within the next day or two.
+<br/>
+<a href="download.html" target="main">Download it now!</a></p>
+        </font>
+      </td>
+    </tr>
+    <tr>
+      <td align="right" valign="top" width="20"><img src="images/arrow.gif" alt="->" border="0" height="16" width="16"></td>
+      <td valign="top"><font face="arial,helvetica,geneva" size="-1"><b> New website! (2004-11-24)</b>
+        <br/>
+<p>Welcome to the latest website for the RDT project. In an effort to help
+promote the project, I decided a new website was a dire need. I know, for one,
+that when I'm looking at a new program I base a large part of the decision on
+the website of the project. If the site offers no screenshots, docs or other
+compelling reason to use the program I typically pass by. Here's hoping we
+increase the the usership and visibility of the priject. We could use the 
+extra hands!</p>
+        </font>
+      </td>
+    </tr>
+  </table>
