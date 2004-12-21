01019dcb7bbbaa8ce11bf7e84ac9991bfc418750
mbarchfe <mbarchfe@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2004-12-21 23:29:49 +0000

    now includes sync of images
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@760 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.webpage/publish.sh
+++ org.rubypeople.rdt.webpage/publish.sh
@@ -1,1 +1,3 @@
-rsync -vu -e "ssh" htdocs/*.php htdocs/.htaccess htdocs/*.css mbarchfe@shell.sf.net:/home/groups/r/ru/rubyeclipse/htdocs
+rsync -vu -e "ssh" htdocs/*.php htdocs/.htaccess htdocs/*.css  mbarchfe@shell.sf.net:/home/groups/r/ru/rubyeclipse/htdocs
+rsync -vu -e "ssh" htdocs/images/*.gif htdocs/images/*.png mbarchfe@shell.sf.net:/home/groups/r/ru/rubyeclipse/htdocs/images
+rsync -vu -e "ssh" htdocs/images/screenshots/*.jpg mbarchfe@shell.sf.net:/home/groups/r/ru/rubyeclipse/htdocs/images/screenshots
