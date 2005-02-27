9deec91d31b8352740a8da6a7af210297eb06fd1
mbarchfe <mbarchfe@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2005-02-27 19:20:36 +0000

    added dontRunTests
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@882 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.build/bootstrap/run.sh
+++ org.rubypeople.rdt.build/bootstrap/run.sh
@@ -14,6 +14,7 @@ ws=${RDT_WS:-motif}
 arch=${RDT_ARCH:-x86}
 usePserver=${RDT_USE_PSERVER:+-DusePserver=true}
 testClean=${RDT_TEST_CLEAN:+-Dclean=true}
+dontRunTests=${RDT_DONT_RUN_TESTS:+-DdontRunTests=true}
 
 #REM reset ant command line args
 ANT_CMD_LINE_ARGS=
@@ -22,6 +23,6 @@ ANT_CMD_LINE_ARGS=
 buildfile=$eclipseDir/plugins/org.eclipse.pde.build_$pdeBuildPluginVersion/scripts/build.xml
 
 echo Starting eclipse in $eclipseDir, $vm
-cmd="$vm -cp $eclipseDir/startup.jar org.eclipse.core.launcher.Main -ws $ws -os $os -application org.eclipse.ant.core.antRunner  -buildfile $buildfile -data $buildDirectory/workspace $verboseAnt $usePserver -Dbasews=$ws -Dbaseos=$os -Dbasearch=$arch -Dbuilder=$bootstrapDir  $testClean  -DjavacFailOnError=true -DbuildDirectory=$buildDirectory -DbaseLocation=$eclipseDir  -DeclipseAutomatedTestHome=$eclipseAutomatedTestHome -Drdt.rubyInterpreter="$rubyInterpreter" -Drdt-tests-workspace=$buildDirectory/workspace-rdt-tests -Ddocbook.root=$docbookRoot"
+cmd="$vm -cp $eclipseDir/startup.jar org.eclipse.core.launcher.Main -ws $ws -os $os -application org.eclipse.ant.core.antRunner  -buildfile $buildfile -data $buildDirectory/workspace $verboseAnt $usePserver $dontRunTests -Dbasews=$ws -Dbaseos=$os -Dbasearch=$arch -Dbuilder=$bootstrapDir  $testClean  -DjavacFailOnError=true -DbuildDirectory=$buildDirectory -DbaseLocation=$eclipseDir  -DeclipseAutomatedTestHome=$eclipseAutomatedTestHome -Drdt.rubyInterpreter="$rubyInterpreter" -Drdt-tests-workspace=$buildDirectory/workspace-rdt-tests -Ddocbook.root=$docbookRoot"
 echo $cmd
 exec $cmd
