944929270c28fb9aa1387510dde1d82ba27d2b5a
cawilliams <cawilliams@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2005-03-05 20:47:39 +0000

    add more view shortcuts, show problems view first at bottom like JDT
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@925 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.ui/src/org/rubypeople/rdt/internal/ui/RdtPerspectiveFactory.java
+++ org.rubypeople.rdt.ui/src/org/rubypeople/rdt/internal/ui/RdtPerspectiveFactory.java
@@ -5,7 +5,8 @@ import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 import org.eclipse.ui.console.IConsoleConstants;
-import org.rubypeople.rdt.ui.actions.RubyActionSetIds;
+import org.eclipse.ui.progress.IProgressConstants;
+import org.rubypeople.rdt.ui.actions.IRubyActionSetIds;
 
 public class RdtPerspectiveFactory implements IPerspectiveFactory {
 
@@ -18,17 +19,39 @@ public class RdtPerspectiveFactory implements IPerspectiveFactory {
 
 		IFolderLayout rubyResourcesArea = layout.createFolder("rubyResourcesArea", IPageLayout.LEFT, (float) 0.26, existingEditorArea);
 		rubyResourcesArea.addView(RubyPlugin.RUBY_RESOURCES_VIEW_ID);
+		rubyResourcesArea.addPlaceholder(IPageLayout.ID_RES_NAV);
 
 		IFolderLayout consoleArea = layout.createFolder("consoleArea", IPageLayout.BOTTOM, (float) 0.75, existingEditorArea);
-		consoleArea.addView(IConsoleConstants.ID_CONSOLE_VIEW);
+		consoleArea.addView(IPageLayout.ID_PROBLEM_VIEW);
+		consoleArea.addPlaceholder(IConsoleConstants.ID_CONSOLE_VIEW);
 		consoleArea.addView(IPageLayout.ID_TASK_LIST);
-		consoleArea.addPlaceholder(IPageLayout.ID_PROBLEM_VIEW);
-
-		layout.addActionSet(IDebugUIConstants.LAUNCH_ACTION_SET);
-		layout.addActionSet(RubyActionSetIds.RUBY_ACTION_SET_ID);
+		consoleArea.addPlaceholder(IProgressConstants.PROGRESS_VIEW_ID);
 
 		IFolderLayout bottomLeft = layout.createFolder("bottomLeft", IPageLayout.BOTTOM, (float) 0.5, "rubyResourcesArea");
 		bottomLeft.addView(IPageLayout.ID_OUTLINE);
+		
+		layout.addActionSet(IDebugUIConstants.LAUNCH_ACTION_SET);
+		layout.addActionSet(IRubyActionSetIds.RUBY_ACTION_SET_ID);
+		layout.addActionSet(IRubyActionSetIds.ID_ELEMENT_CREATION_ACTION_SET);
+		layout.addActionSet(IPageLayout.ID_NAVIGATE_ACTION_SET);
+		
+		// views - debugging
+		layout.addShowViewShortcut(IConsoleConstants.ID_CONSOLE_VIEW);
+
+		// views - standard workbench
+		layout.addShowViewShortcut(IPageLayout.ID_OUTLINE);
+		layout.addShowViewShortcut(IPageLayout.ID_PROBLEM_VIEW);
+		layout.addShowViewShortcut(IPageLayout.ID_RES_NAV);
+				
+		// new actions - Java project creation wizard
+		layout.addNewWizardShortcut("org.eclipse.jdt.ui.wizards.NewPackageCreationWizard"); //$NON-NLS-1$
+		layout.addNewWizardShortcut("org.eclipse.jdt.ui.wizards.NewClassCreationWizard"); //$NON-NLS-1$
+		layout.addNewWizardShortcut("org.eclipse.jdt.ui.wizards.NewInterfaceCreationWizard"); //$NON-NLS-1$
+		layout.addNewWizardShortcut("org.eclipse.jdt.ui.wizards.EnumCreationWizard"); //$NON-NLS-1$
+		layout.addNewWizardShortcut("org.eclipse.jdt.ui.wizards.NewSourceFolderCreationWizard");	 //$NON-NLS-1$
+		layout.addNewWizardShortcut("org.eclipse.jdt.ui.wizards.NewSnippetFileCreationWizard"); //$NON-NLS-1$
+		layout.addNewWizardShortcut("org.eclipse.ui.wizards.new.folder");//$NON-NLS-1$
+		layout.addNewWizardShortcut("org.eclipse.ui.wizards.new.file");//$NON-NLS-1$
 	}
 
 }
