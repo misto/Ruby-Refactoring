ac96b329aa3de4b18fa1e0aa6ca6338998f99207
cawilliams <cawilliams@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2005-03-05 20:47:57 +0000

    rename interface
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@926 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.ui/src/org/rubypeople/rdt/ui/actions/IRubyActionSetIds.java
+++ org.rubypeople.rdt.ui/src/org/rubypeople/rdt/ui/actions/IRubyActionSetIds.java
@@ -1,8 +1,9 @@
 package org.rubypeople.rdt.ui.actions;
 
-public interface RubyActionSetIds {
+public interface IRubyActionSetIds {
 	/**
 	 * (value <code>"org.rubypeople.rdt.ui.rubyActionSet"</code>).
 	 */
 	public static final String RUBY_ACTION_SET_ID = "org.rubypeople.rdt.ui.rubyActionSet";
+	public static final String ID_ELEMENT_CREATION_ACTION_SET = "org.rubypeople.rdt.ui.RubyElementCreationActionSet";
 }
