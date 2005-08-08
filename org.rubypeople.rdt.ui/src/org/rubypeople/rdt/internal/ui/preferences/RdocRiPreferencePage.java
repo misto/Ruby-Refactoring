0f4c6243c26a1184b4614ae332dd8313f254f7cc
zdennis <zdennis@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2005-08-08 23:25:28 +0000

    Added preference page to allow user to set the path to the location of rdoc and ri programs. RiView relies on this.
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@1020 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

---
+++ org.rubypeople.rdt.ui/src/org/rubypeople/rdt/internal/ui/preferences/RdocRiPreferencePage.java
@@ -0,0 +1,84 @@
+package org.rubypeople.rdt.internal.ui.preferences;
+
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.FileFieldEditor;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+import org.rubypeople.rdt.internal.ui.RubyPlugin;
+import org.rubypeople.rdt.internal.ui.util.Rdoc;
+import org.rubypeople.rdt.internal.ui.util.Ri;
+import org.rubypeople.rdt.ui.PreferenceConstants;
+
+/**
+ * This class represents a preference page that
+ * is contributed to the Preferences dialog. By 
+ * subclassing <samp>FieldEditorPreferencePage</samp>, we
+ * can use the field support built into JFace that allows
+ * us to create a page that is small and knows how to 
+ * save, restore and apply itself.
+ * <p>
+ * This page is used to modify preferences only. They
+ * are stored in the preference store that belongs to
+ * the main plug-in class. That way, preferences can
+ * be accessed directly via the preference store.
+ */
+
+public class RdocRiPreferencePage
+	extends FieldEditorPreferencePage
+	implements IWorkbenchPreferencePage {
+
+	public RdocRiPreferencePage() {
+		super(GRID);
+		setPreferenceStore(RubyPlugin.getDefault().getPreferenceStore());
+		setDefaults();
+		setDescription("Settings for Rdoc and Ri.");
+	}
+		
+	private void setDefaults(){
+		IPreferenceStore store = RubyPlugin.getDefault().getPreferenceStore();
+		store.setDefault( PreferenceConstants.RDOC_PATH, 
+			Rdoc.getDefaultPath().toOSString() );
+		store.setDefault( PreferenceConstants.RI_PATH,
+			Ri.getDefaultPath().toOSString() );
+	}
+	
+	/**
+	 * Creates the field editors. Field editors are abstractions of
+	 * the common GUI blocks needed to manipulate various types
+	 * of preferences. Each field editor knows how to save and
+	 * restore itself.
+	 */
+	public void createFieldEditors() {
+		addField( new FileFieldEditor( PreferenceConstants.RDOC_PATH,
+				"&Rdoc directoy path:", getFieldEditorParent() ) );
+		
+		addField( new FileFieldEditor( PreferenceConstants.RI_PATH,
+				"R&i directoy path:", getFieldEditorParent() ) );		
+//		
+//		addField(new DirectoryFieldEditor(PreferenceConstants.P_PATH, 
+//				"&Directory preference:", getFieldEditorParent()));
+//		addField(
+//			new BooleanFieldEditor(
+//				PreferenceConstants.P_BOOLEAN,
+//				"&An example of a boolean preference",
+//				getFieldEditorParent()));
+//
+//		addField(new RadioGroupFieldEditor(
+//				PreferenceConstants.P_CHOICE,
+//			"An example of a multiple-choice preference",
+//			1,
+//			new String[][] { { "&Choice 1", "choice1" }, {
+//				"C&hoice 2", "choice2" }
+//		}, getFieldEditorParent()));
+//		addField(
+//			new StringFieldEditor(PreferenceConstants.P_STRING, "A &text preference:", getFieldEditorParent()));
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.IWorkbenchPreferencePage#init(org.eclipse.ui.IWorkbench)
+	 */
+	public void init(IWorkbench workbench) {
+	}
+	
+}
