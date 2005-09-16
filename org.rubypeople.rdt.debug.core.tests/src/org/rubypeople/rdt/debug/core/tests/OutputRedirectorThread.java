51c90b3eaad90579ca36cf0c4abbe192b3f4f316
mbarchfe <mbarchfe@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2005-09-16 20:51:30 +0000

    stores lastLine for better error reporting from TC_DebuggerCommunicationTest
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@1070 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.debug.core.tests/src/org/rubypeople/rdt/debug/core/tests/OutputRedirectorThread.java
+++ org.rubypeople.rdt.debug.core.tests/src/org/rubypeople/rdt/debug/core/tests/OutputRedirectorThread.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 
 public class OutputRedirectorThread extends Thread {
 	private InputStream inputStream;
+	private String lastLine = "No output." ;
 	public OutputRedirectorThread(InputStream aInputStream) {
 		inputStream = aInputStream;
 	}
@@ -14,15 +15,19 @@ public class OutputRedirectorThread extends Thread {
 	public void run() {
 		System.out.println("OutputRedirectorThread started.");
 		BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
-
-		String line;
+		String line ;
 		try {
 			while ((line = br.readLine()) != null) {
 				System.out.println("RUBY: " + line);
+				lastLine = line ;
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		System.out.println("OutputRedirectorThread stopped.");
+	}
+	
+	public String getLastLine() {
+		return lastLine ;
 	}
 }
