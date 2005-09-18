8e0244c998298a2f330ef37bc51fad68c150d9f1
mbarchfe <mbarchfe@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2005-09-18 14:20:53 +0000

    splittet RdtAllTests into unit and functional tests
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@1083 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

---
+++ org.rubypeople.rdt.tests.all/src/org/rubypeople/rdt/tests/all/TS_RdtAllUnitTests.java
@@ -0,0 +1,35 @@
+package org.rubypeople.rdt.tests.all;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.rubypeople.rdt.internal.core.TS_Core;
+import org.rubypeople.rdt.internal.formatter.TC_CodeFormatter;
+import org.rubypeople.rdt.debug.core.tests.TS_Debug;
+import org.rubypeople.rdt.internal.debug.ui.tests.launcher.TS_Launcher;
+import org.rubypeople.rdt.internal.launching.tests.TS_Launching;
+import org.rubypeople.rdt.internal.ui.TS_UiTests;
+
+public class TS_RdtAllUnitTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite("RDT all unit tests");
+		//$JUnit-BEGIN$
+
+		// org.rubypeople.rdt.core.tests
+		suite.addTest(TS_Core.suite());
+		suite.addTestSuite(TC_CodeFormatter.class) ;		
+		
+		// org.rubypeople.rdt.launching.tests
+		suite.addTest(TS_Launching.suite());
+		
+		// org.rubypeople.rdt.ui.tests
+		suite.addTest(TS_UiTests.suite());
+		
+		// org.rubypeople.rdt.debug.ui.tests
+		suite.addTest(TS_Launcher.suite());
+
+		//$JUnit-END$
+		return suite;
+	}
+}
