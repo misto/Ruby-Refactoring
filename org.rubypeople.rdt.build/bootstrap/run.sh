832b259674a7c3ec254bd740e3639e75b1b8764a
mbarchfe <mbarchfe@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2005-09-18 10:21:29 +0000

    define noclean instead of clean; new variable buildTarget
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@1077 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.build/bootstrap/run.sh
+++ org.rubypeople.rdt.build/bootstrap/run.sh
@@ -13,7 +13,8 @@ os=${RDT_OS:-linux}
 ws=${RDT_WS:-motif}
 arch=${RDT_ARCH:-x86}
 usePserver=${RDT_USE_PSERVER:+-DusePserver=true}
-testClean=${RDT_TEST_CLEAN:+-Dclean=true}
+# the default is to clean up before testing, it can be avoided by defining noclean
+testNoclean=${RDT_TEST_NOCLEAN:+-Dnoclean=true}
 dontRunTests=${RDT_DONT_RUN_TESTS:+-DdontRunTests=true}
 
 #REM reset ant command line args
@@ -21,8 +22,11 @@ ANT_CMD_LINE_ARGS=
 
 
 buildfile=$eclipseDir/plugins/org.eclipse.pde.build_$pdeBuildPluginVersion/scripts/build.xml
+# buildTarget can be one of the build targets defined in $buildfile: 
+# main, preBuild, fetch, generate, process, assemble, package, postBuild, clean
+buildTarget=${RDT_BUILD_TARGET:+main}
 
 echo Starting eclipse in $eclipseDir, $vm
-cmd="$vm -cp $eclipseDir/startup.jar org.eclipse.core.launcher.Main -ws $ws -os $os -application org.eclipse.ant.core.antRunner  -buildfile $buildfile -data $buildDirectory/workspace $verboseAnt $usePserver $dontRunTests -Dbasews=$ws -Dbaseos=$os -Dbasearch=$arch -Dbuilder=$bootstrapDir  $testClean  -DjavacFailOnError=true -DbuildDirectory=$buildDirectory -DbaseLocation=$eclipseDir  -DeclipseAutomatedTestHome=$eclipseAutomatedTestHome -Drdt.rubyInterpreter="$rubyInterpreter" -Drdt-tests-workspace=$buildDirectory/workspace-rdt-tests -Ddocbook.root=$docbookRoot"
+cmd="$vm -cp $eclipseDir/startup.jar org.eclipse.core.launcher.Main -ws $ws -os $os -application org.eclipse.ant.core.antRunner -buildfile $buildfile $buildTarget -data $buildDirectory/workspace $verboseAnt $usePserver $dontRunTests -Dbasews=$ws -Dbaseos=$os -Dbasearch=$arch -Dbuilder=$bootstrapDir  $testNoclean  -DjavacFailOnError=true -DbuildDirectory=$buildDirectory -DbaseLocation=$eclipseDir  -DeclipseAutomatedTestHome=$eclipseAutomatedTestHome -Drdt.rubyInterpreter="$rubyInterpreter" -Drdt-tests-workspace=$buildDirectory/workspace-rdt-tests -Ddocbook.root=$docbookRoot"
 echo $cmd
 exec $cmd
