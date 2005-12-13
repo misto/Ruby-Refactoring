a46f14d590bb97b88ca7f62807838a8df118ddf7
mekent <mekent@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2005-12-13 15:19:21 +0000

    added rubyTemplateProvider extension point for RadRails use
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@1235 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.ui/src/org/rubypeople/rdt/internal/ui/rubyeditor/templates/RubyTemplateAccess.java
+++ org.rubypeople.rdt.ui/src/org/rubypeople/rdt/internal/ui/rubyeditor/templates/RubyTemplateAccess.java
@@ -11,12 +11,23 @@
 package org.rubypeople.rdt.internal.ui.rubyeditor.templates;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtension;
+import org.eclipse.core.runtime.IExtensionPoint;
+import org.eclipse.core.runtime.IExtensionRegistry;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.text.templates.ContextTypeRegistry;
+import org.eclipse.jface.text.templates.persistence.TemplatePersistenceData;
 import org.eclipse.jface.text.templates.persistence.TemplateStore;
 import org.eclipse.ui.editors.text.templates.ContributionContextTypeRegistry;
 import org.eclipse.ui.editors.text.templates.ContributionTemplateStore;
 import org.rubypeople.rdt.internal.ui.RubyPlugin;
+import org.rubypeople.rdt.ui.extensions.IRubyTemplateProvider;
 
 
 public class RubyTemplateAccess {
@@ -59,8 +70,72 @@ public class RubyTemplateAccess {
 			} catch (IOException e) {
 				RubyPlugin.log(e);
 			}
+			
+			// Load extension templates
+			TemplatePersistenceData[] tempData = getExtensionTemplateData();
+			for(int i = 0; i < tempData.length; i++) {
+				fStore.add(tempData[i]);
+			}
 		}
 		return fStore;
+	}
+	
+	/**
+	 * Finds all extensions to the rubyTemplateProvider extension point and return their template data.
+	 * 
+	 * @return an array of TemplatePersistenceData
+	 */
+	private TemplatePersistenceData[] getExtensionTemplateData() {
+		List extensions = new ArrayList();
+		IExtensionRegistry reg = Platform.getExtensionRegistry();
+		
+		IExtensionPoint[] points = reg.getExtensionPoints(RubyPlugin.PLUGIN_ID);
+		IExtensionPoint point = null;
+		
+		// Search the extension registry for the rubyTemplateProvider extension point
+		if(points != null){
+			for (int i = 0; i < points.length; i++) {
+				IExtensionPoint currentPoint = points[i];
+				if(currentPoint.getUniqueIdentifier().endsWith("rubyTemplateProvider")){
+					point = currentPoint;
+					break;
+				}				
+			}
+			
+			// Find all extensions of the point
+			if(point != null){
+				IExtension[] exts = point.getExtensions();
+				
+				IRubyTemplateProvider prov = null;
+				
+				// Get the implementing class of the extension
+				for (int i = 0; i < exts.length; i++) {
+					IConfigurationElement[] elem = exts[i].getConfigurationElements();
+					String attrs[] = elem[0].getAttributeNames();
+					try {
+						Object tempProv = elem[0].createExecutableExtension("class");
+						if (tempProv instanceof IRubyTemplateProvider) {
+							prov = (IRubyTemplateProvider) tempProv;
+							extensions.add(prov);
+						}
+					} catch (CoreException e) {
+						RubyPlugin.log(e);
+					}
+				}
+			}
+		}
+		
+		// Get the template data from the extensions
+		if(extensions.size() > 0){
+			for(int i=0; i< extensions.size(); i++){
+				IRubyTemplateProvider currentProvider = (IRubyTemplateProvider) extensions.get(i);
+				TemplatePersistenceData[] templates = currentProvider.getTemplateData();
+				if(templates != null){
+					return templates;
+				}					
+			}
+		} 	
+		return null;
 	}
 
 	/**
