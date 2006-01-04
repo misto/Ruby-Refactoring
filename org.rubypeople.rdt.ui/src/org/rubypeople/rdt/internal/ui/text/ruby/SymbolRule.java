242da55d4037753daa128a729e3ebe4f079b9ede
cawilliams <cawilliams@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2006-01-04 00:00:22 +0000

    fix ticket #63 (though there's still work to do to get this rule 100% right)
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@1258 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.ui/src/org/rubypeople/rdt/internal/ui/text/ruby/SymbolRule.java
+++ org.rubypeople.rdt.ui/src/org/rubypeople/rdt/internal/ui/text/ruby/SymbolRule.java
@@ -10,16 +10,30 @@ import org.eclipse.jface.text.rules.IToken;
  */
 public class SymbolRule extends SingleCharacterPrefixRule {
 
-    private static final char PREFIX = ':';
+	private static final char PREFIX = ':';
 
-    public SymbolRule(IToken token) {
-        super(PREFIX, token, 2, Integer.MAX_VALUE);
-    }
-    
-    protected boolean isValidCharacter(int c, int index) {
-      if (!super.isValidCharacter(c, index)) return false;
-      if (((char)c) == ':') return false; // other than first character (prefix) symbols can't contain another colon     
-      return true;
-    }
+	public SymbolRule(IToken token) {
+		super(PREFIX, token, 2, Integer.MAX_VALUE);
+	}
+
+	protected boolean isValidCharacter(int c, int index) {
+		if (!super.isValidCharacter(c, index))
+			return false;
+		char theChar = (char) c;
+		switch (theChar) {
+		case ':': // other than first character (prefix) symbols can't contain
+					// another colon
+		case '[': // FIXME This isn't always the case. There's special method names containing brackets that is valid (only the particular sequence)
+		case ']':
+		case '{':
+		case '}':
+		case '(':
+		case ')':
+			return false;
+
+		default:
+			return true;
+		}
+	}
 
 }
