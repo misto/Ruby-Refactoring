630568d680f8ce970154dbd12f36c75a26be6e72
mbarchfe <mbarchfe@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2006-01-08 15:19:49 +0000

    copy test result file to correct place, purge download and updatesite, do not use verbose ANT
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@1269 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.build/cruiseControl/purgeNightlyBuildWebSite.sh
+++ org.rubypeople.rdt.build/cruiseControl/purgeNightlyBuildWebSite.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ssh -l rubypeople download.rubypeople.org "rm -r ~/subdomains/updatesite/httpdocs/nightly;mkdir ~/subdomains/updatesite/httpdocs/nightly"
+ssh -l rubypeople download.rubypeople.org "rm -r ~/subdomains/updatesite/httpdocs/nightly; mkdir ~/subdomains/updatesite/httpdocs/nightly; rm -r ~/subdomains/download/httpdocs/nightly ; mkdir ~/subdomains/download/httpdocs/nightly"
