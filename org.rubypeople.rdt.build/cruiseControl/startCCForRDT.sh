72ca1a479de106e6074bfcdee6ff240c6fc450dc
mbarchfe <mbarchfe@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2006-08-26 18:01:41 +0000

    start jmx
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@1591 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.build/cruiseControl/startCCForRDT.sh
+++ org.rubypeople.rdt.build/cruiseControl/startCCForRDT.sh
@@ -1,5 +1,4 @@
-#! /bin/sh
+#!/bin/bash
 Xvfb :1 &
 export DISPLAY=localhost:1
-#cd ~/cruisecontrol-bin-2.5
-sh ../cruisecontrol-bin-2.5/cruisecontrol.sh -webport 8090 $@
+~/cruisecontrol-bin-2.5/cruisecontrol.sh -webport 8080 -jmxport 8000 $@
