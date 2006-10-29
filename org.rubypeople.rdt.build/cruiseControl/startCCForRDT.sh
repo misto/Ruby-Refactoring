2005e6fb75b2ab04d904b0533ac83051e7f77788
mbarchfe <mbarchfe@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2006-10-29 23:34:23 +0000

    use symblink to cc dir
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@1631 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.build/cruiseControl/startCCForRDT.sh
+++ org.rubypeople.rdt.build/cruiseControl/startCCForRDT.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 Xvfb :1 &
 export DISPLAY=localhost:1
-~/cruisecontrol-bin-2.5/cruisecontrol.sh -webport 8080 -jmxport 8000 $@
+~/cruisecontrol/cruisecontrol.sh -webport 8080 -jmxport 8000 $@
