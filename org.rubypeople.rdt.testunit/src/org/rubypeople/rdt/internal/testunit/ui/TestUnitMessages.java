2b522b18ce4046f264cac07fbf2e662d27dc3787
cawilliams <cawilliams@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2006-12-15 15:39:49 +0000

    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@1711 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

---
+++ org.rubypeople.rdt.testunit/src/org/rubypeople/rdt/internal/testunit/ui/TestUnitMessages.java
@@ -0,0 +1,39 @@
+package org.rubypeople.rdt.internal.testunit.ui;
+
+import java.text.MessageFormat;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+public class TestUnitMessages {
+
+	private static final String BUNDLE_NAME= "org.rubypeople.rdt.internal.testunit.ui.TestUnitMessages"; //$NON-NLS-1$
+
+	private static final ResourceBundle RESOURCE_BUNDLE= ResourceBundle.getBundle(BUNDLE_NAME);
+
+	private TestUnitMessages() {
+	}
+
+	/**
+	 * Gets a string from the resource bundle and formats it with the argument
+	 * 
+	 * @param key	the string used to get the bundle value, must not be null
+	 */
+	public static String getFormattedString(String key, Object arg) {
+		return MessageFormat.format(getString(key), new Object[] { arg });
+	}
+
+	/**
+	 * Gets a string from the resource bundle and formats it with arguments
+	 */	
+	public static String getFormattedString(String key, Object[] args) {
+		return MessageFormat.format(getString(key), args);
+	}
+
+	public static String getString(String key) {
+		try {
+			return RESOURCE_BUNDLE.getString(key);
+		} catch (MissingResourceException e) {
+			return '!' + key + '!';
+		}
+	}
+}
