f73eca6e7cd75b08a8129c99b84035bd4905b18d
cawilliams <cawilliams@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2007-01-15 14:19:08 +0000

    fix broken tests
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@1768 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.ui.tests/src/org/rubypeople/rdt/internal/ui/TC_ResourceAdapterFactory.java
+++ org.rubypeople.rdt.ui.tests/src/org/rubypeople/rdt/internal/ui/TC_ResourceAdapterFactory.java
@@ -1,19 +1,20 @@
 package org.rubypeople.rdt.internal.ui;
 
-import junit.framework.TestCase;
-
-import org.rubypeople.eclipse.shams.resources.ShamFile;
-import org.rubypeople.eclipse.shams.resources.ShamProject;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
 import org.rubypeople.rdt.core.IRubyElement;
 import org.rubypeople.rdt.core.IRubyProject;
 import org.rubypeople.rdt.core.IRubyScript;
-import org.rubypeople.rdt.core.RubyCore;
+import org.rubypeople.rdt.core.tests.ModifyingResourceTest;
 import org.rubypeople.rdt.internal.core.RubyProject;
 import org.rubypeople.rdt.internal.core.RubyScript;
 
-public class TC_ResourceAdapterFactory extends TestCase {
+public class TC_ResourceAdapterFactory extends ModifyingResourceTest {
 
+	private static final String PROJECT_NAME = "adapterTest";
 	private ResourceAdapterFactory factory;
+	private IProject project;
 	
 	public TC_ResourceAdapterFactory(String name) {
 		super(name);
@@ -21,24 +22,30 @@ public class TC_ResourceAdapterFactory extends TestCase {
 	
 	protected void setUp() throws Exception {
 		super.setUp();
+		this.project = createProject(PROJECT_NAME);
 		factory = new ResourceAdapterFactory();	
 	}
+	
+	@Override
+	protected void tearDown() throws Exception {
+		super.tearDown();
+		deleteProject(PROJECT_NAME);
+	}
 
-	public void testGetAdapterForRBFile() {
-		ShamFile file = new ShamFile("mustBeA.rb");
+	public void testGetAdapterForRBFile() throws CoreException {
+		IFile file = createFile(PROJECT_NAME + "/mustBeA.rb", "");
 		assertEquals(RubyScript.class, factory.getAdapter(file, IRubyElement.class).getClass());
 		assertTrue(factory.getAdapter(file, IRubyElement.class) instanceof IRubyScript);
 	}
 	
-	public void testGetAdapterForRBWFile() {
-		ShamFile file = new ShamFile("mustBeA.rbw");
+	public void testGetAdapterForRBWFile() throws CoreException {		
+		IFile file = createFile(PROJECT_NAME + "/mustBeA.rbw", "");
 		assertEquals(RubyScript.class, factory.getAdapter(file, IRubyElement.class).getClass());
 		assertTrue(factory.getAdapter(file, IRubyElement.class) instanceof IRubyScript);
 	}
 
-	public void testGetAdapterForProject() {
-		ShamProject project = new ShamProject("AProject");
-		project.addNature(RubyCore.NATURE_ID);
+	public void testGetAdapterForProject() throws CoreException {
+		addRubyNature(PROJECT_NAME);
 		assertEquals(RubyProject.class, factory.getAdapter(project, IRubyElement.class).getClass());
 		assertTrue(factory.getAdapter(project, IRubyElement.class) instanceof IRubyProject);
 	}
