f40fe55d138de0d56092e17581101078108110b5
cawilliams <cawilliams@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2007-01-19 21:11:46 +0000

    not needed
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@1823 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.launching/src/org/rubypeople/rdt/internal/launching/CommandExecutor.java
+++
@@ -1,8 +0,0 @@
-package org.rubypeople.rdt.internal.launching;
-
-import java.io.File;
-import java.io.IOException;
-
-public interface CommandExecutor {
-    public Process exec(String[] command, File workingDirectory) throws IOException;
-}
