e5aa1207d27cca52ce7d97e63c95d2222f68eca7
mbarchfe <mbarchfe@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2007-01-31 22:28:36 +0000

    Added better abstraction for debugger access since we need two socket connections for rubydebug
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@1901 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

---
+++ org.rubypeople.rdt.debug.core/src/org/rubypeople/rdt/internal/debug/core/commands/ClassicDebuggerConnection.java
@@ -0,0 +1,26 @@
+package org.rubypeople.rdt.internal.debug.core.commands;
+
+import java.io.IOException;
+
+import org.rubypeople.rdt.internal.debug.core.DebuggerNotFoundException;
+import org.rubypeople.rdt.internal.debug.core.parsing.SuspensionReader;
+
+public class ClassicDebuggerConnection extends AbstractDebuggerConnection {
+
+	public ClassicDebuggerConnection(int port) {
+		super(port);
+	}
+
+	@Override
+	public void connect() throws DebuggerNotFoundException, IOException {
+		createCommandConnection();
+	}
+
+	@Override
+	public SuspensionReader start() throws DebuggerNotFoundException, IOException {
+		StepCommand stepCommand = new StepCommand("cont");
+		stepCommand.execute(this) ;
+		return stepCommand.getSuspensionReader() ;
+	}
+
+}
---
+++ org.rubypeople.rdt.debug.core/src/org/rubypeople/rdt/internal/debug/core/commands/RubyDebugConnection.java
@@ -0,0 +1,109 @@
+package org.rubypeople.rdt.internal.debug.core.commands;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import org.rubypeople.rdt.internal.debug.core.DebuggerNotFoundException;
+import org.rubypeople.rdt.internal.debug.core.RdtDebugCorePlugin;
+import org.rubypeople.rdt.internal.debug.core.model.RubyProcessingException;
+import org.rubypeople.rdt.internal.debug.core.parsing.AbstractReadStrategy;
+import org.rubypeople.rdt.internal.debug.core.parsing.MultiReaderStrategy;
+import org.rubypeople.rdt.internal.debug.core.parsing.SuspensionReader;
+import org.xmlpull.v1.XmlPullParser;
+
+public class RubyDebugConnection extends AbstractDebuggerConnection {
+
+	private Socket controlSocket ;
+	private MultiReaderStrategy controlReadStrategy;
+	private PrintWriter controlWriter;
+	private String rdebugExtensionPath ;
+	public RubyDebugConnection(String rdebugExtensionPath, int port) {
+		super(port);
+		this.rdebugExtensionPath = rdebugExtensionPath +  File.separatorChar + "rdebugExtension.rb";
+	}
+
+	@Override
+	public void connect() throws DebuggerNotFoundException, IOException{
+		createControlConnection() ;
+		
+		String expression = "eval require '" + rdebugExtensionPath + "'";
+		EvalCommand command = new EvalCommand(expression, true) ;
+		command.execute(this) ;
+		String evalResult = null;
+		try {
+			evalResult = command.getEvalReader().readEvalResult();
+		} catch (RubyProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (evalResult == null || !evalResult.equals("true")) {
+			// TODO: go on ?
+			throw new DebuggerNotFoundException("Could not add extension to ruby debug") ;
+		}
+	}
+
+	@Override
+	public SuspensionReader start() throws DebuggerNotFoundException, IOException {
+		createCommandConnection() ;
+		return new SuspensionReader(getCommandReadStrategy()) ;
+	}
+	
+	@Override
+	public AbstractReadStrategy sendCommand(AbstractCommand command) throws DebuggerNotFoundException, IOException {
+		AbstractReadStrategy result = null ;
+		if (command.isControl()) {
+			result =sendControlCommand(command) ; 
+		} else {
+			result = super.sendCommand(command);
+		}
+		return result ;
+	}
+	
+	private AbstractReadStrategy sendControlCommand(AbstractCommand command) throws IOException {
+		if (!isControlPortConnected()) {
+			throw new IllegalStateException(command + " could not be executed since control socket is not opened.") ;
+		}
+		RdtDebugCorePlugin.debug("Sending control command: " + command.getCommand()) ;
+		getControlWriter().println(command.getCommand()) ;
+		return getControlReadStrategy() ;
+	}
+	
+	private PrintWriter getControlWriter() throws IOException {
+		if (controlWriter == null) {
+			controlWriter = new PrintWriter(getControlSocket().getOutputStream(), true);
+		}
+		return controlWriter;
+	}
+	
+	protected boolean isControlPortConnected() {
+		return controlReadStrategy != null;  
+	}
+	
+	protected void createControlConnection() throws DebuggerNotFoundException, IOException {
+		Socket socket = getControlSocket() ;
+		XmlPullParser xpp = createXpp(socket) ;
+		controlReadStrategy = new MultiReaderStrategy(xpp) ;
+	}
+	
+	private Socket getControlSocket() throws IOException {
+		if (controlSocket == null) {
+			controlSocket = acquireSocket(getCommandPort() +  1) ;
+		}
+		return controlSocket ;
+	}
+
+	public MultiReaderStrategy getControlReadStrategy() {
+		return controlReadStrategy;
+	}
+	
+	@Override
+	public void exit() throws IOException {
+		super.exit();
+		GenericCommand command = new GenericCommand("exit", true) ;
+		command.execute(this) ;
+		controlSocket.close() ;
+	}
+
+}
