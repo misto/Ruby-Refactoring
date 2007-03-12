a88954c01b32af2b1c50931f5d961fd72e61fbf5
callandor1983 <callandor1983@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2007-03-12 11:54:59 +0000

    removed spare argument form constructor form RefactoringConditionChecker.
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@2132 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.refactoring.tests/src/org/rubypeople/rdt/refactoring/tests/core/TC_RefactoringConditionChecker.java
+++ org.rubypeople.rdt.refactoring.tests/src/org/rubypeople/rdt/refactoring/tests/core/TC_RefactoringConditionChecker.java
@@ -10,9 +10,11 @@ import org.rubypeople.rdt.refactoring.documentprovider.StringDocumentProvider;
 
 public class TC_RefactoringConditionChecker extends TestCase {
 	private final class TestConditionChecker extends RefactoringConditionChecker {
-		private TestConditionChecker(IDocumentProvider provider, IRefactoringConfig config) {
-			super(provider, config);
+		private TestConditionChecker(final IDocumentProvider provider) {
+			super(getDefaultConfig(provider));
 		}
+
+		
 
 		@Override
 		public void init(Object configObj) {				
@@ -25,7 +27,7 @@ public class TC_RefactoringConditionChecker extends TestCase {
 
 	public void testSyntaxErrors() {
 		
-		RefactoringConditionChecker checker = new TestConditionChecker(new StringDocumentProvider("errorous_dummy_doc.rb", "class Test; en"), null);
+		RefactoringConditionChecker checker = new TestConditionChecker(new StringDocumentProvider("errorous_dummy_doc.rb", "class Test; en"));
 		assertEquals(1, checker.getInitialMessages().get(IRefactoringConditionChecker.ERRORS).size());
 		assertEquals(0, checker.getInitialMessages().get(IRefactoringConditionChecker.WARNING).size());
 	}
@@ -35,11 +37,22 @@ public class TC_RefactoringConditionChecker extends TestCase {
 		StringDocumentProvider stringDocumentProvider = new StringDocumentProvider("dummy_doc_wit_errorous_include.rb", "class Test; end");
 		stringDocumentProvider.addFile("other", "class Test; en");
 		
-		RefactoringConditionChecker checker = new TestConditionChecker(stringDocumentProvider, null);
+		RefactoringConditionChecker checker = new TestConditionChecker(stringDocumentProvider);
 		assertEquals(0, checker.getInitialMessages().get(IRefactoringConditionChecker.ERRORS).size());
 		assertEquals(0, checker.getInitialMessages().get(IRefactoringConditionChecker.WARNING).size());
 
 		assertEquals(0, checker.getFinalMessages().get(IRefactoringConditionChecker.ERRORS).size());
 		assertEquals(1, checker.getFinalMessages().get(IRefactoringConditionChecker.WARNING).size());
+	}
+	
+	private static IRefactoringConfig getDefaultConfig(final IDocumentProvider provider) {
+		return new IRefactoringConfig() {
+
+			public IDocumentProvider getDocumentProvider() {
+				return provider;
+			}
+
+			public void setDocumentProvider(IDocumentProvider doc) {
+			}};
 	}
 }
