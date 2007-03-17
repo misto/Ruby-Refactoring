fc77507ec616b99ce975ad00147fbf16d80afa0d
cawilliams <cawilliams@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2007-03-17 15:02:31 +0000

    add more hooks to perform analysis
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@2198 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.core/src/org/rubypeople/rdt/internal/core/parser/warnings/RubyLintVisitor.java
+++ org.rubypeople.rdt.core/src/org/rubypeople/rdt/internal/core/parser/warnings/RubyLintVisitor.java
@@ -1,6 +1,14 @@
 package org.rubypeople.rdt.internal.core.parser.warnings;
 
+import org.jruby.ast.BlockNode;
+import org.jruby.ast.ClassNode;
+import org.jruby.ast.DefnNode;
+import org.jruby.ast.DefsNode;
+import org.jruby.ast.IfNode;
+import org.jruby.ast.ModuleNode;
 import org.jruby.ast.Node;
+import org.jruby.ast.SClassNode;
+import org.jruby.ast.WhenNode;
 import org.jruby.ast.visitor.AbstractVisitor;
 import org.jruby.evaluator.Instruction;
 import org.jruby.lexer.yacc.ISourcePosition;
@@ -47,7 +55,55 @@ public abstract class RubyLintVisitor extends AbstractVisitor {
 	 * @return a String key
 	 */
 	abstract protected String getOptionKey();
+	
+	/**
+	 * Meant to be overriden by classes needing to perform some action when a class definition was exited.
+	 * @param iVisited
+	 */
+	public void exitClassNode(ClassNode iVisited) {}
+	
+	/**
+	 * To be overriden by subclasses who need to run particular behavior/code when exiting a method definition.
+	 * @param iVisited
+	 */
+	public void exitDefnNode(DefnNode iVisited) {}
+	
+	/**
+	 * To be overriden by subclasses who need to run particular behavior/code when exiting a singleton method definition.
+	 * @param iVisited
+	 */
+	public void exitIfNode(IfNode iVisited) {}
+	
+	/**
+	 * To be overriden by subclasses who need to run particular behavior/code when exiting a singleton method definition.
+	 * @param iVisited
+	 */
+	public void exitBlockNode(BlockNode iVisited) {}
+	
+	/**
+	 * To be overriden by subclasses who need to run particular behavior/code when exiting a singleton method definition.
+	 * @param iVisited
+	 */
+	public void exitDefsNode(DefsNode iVisited) {}
+	
+	/**
+	 * To be overriden by subclasses who need to run particular behavior/code when exiting a singleton method definition.
+	 * @param iVisited
+	 */
+	public void exitModuleNode(ModuleNode iVisited) {}
 
+	/**
+	 * To be overriden by subclasses who need to run particular behavior/code when exiting a singleton method definition.
+	 * @param iVisited
+	 */
+	public void exitWhenNode(WhenNode iVisited) {}
+	
+	/**
+	 * To be overriden by subclasses who need to run particular behavior/code when exiting a singleton method definition.
+	 * @param iVisited
+	 */
+	public void exitSClassNode(SClassNode iVisited) {}
+	
 	protected int getProblemID() {
 		return IProblem.Uncategorized;
 	}
