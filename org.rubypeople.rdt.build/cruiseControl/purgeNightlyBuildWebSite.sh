0e810c763263ed5f989e04ebb3a84c027498034b
mbarchfe <mbarchfe@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2007-04-29 08:56:55 +0000

    removed entries for old download and updatsite
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@2390 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.build/cruiseControl/purgeNightlyBuildWebSite.sh
+++ org.rubypeople.rdt.build/cruiseControl/purgeNightlyBuildWebSite.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-ssh -l rubypeople download.rubypeople.org "rm -r ~/subdomains/updatesite/httpdocs/nightly; mkdir ~/subdomains/updatesite/httpdocs/nightly; rm -r ~/subdomains/download/httpdocs/nightly ; mkdir ~/subdomains/download/httpdocs/nightly"
 ssh -l mbarchfeld www.aptana.org "rm -r /var/websites/updatesite.rubypeople.org/html/nightly; mkdir /var/websites/updatesite.rubypeople.org/html/nightly; rm -r /var/websites/download.rubypeople.org/html/nightly ; mkdir /var/websites/download.rubypeople.org/html/nightly"
  
 
