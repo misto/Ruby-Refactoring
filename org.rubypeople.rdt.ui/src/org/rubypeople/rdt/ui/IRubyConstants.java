58c6d5c85cc7686365b711cc253be14cea940cd0
cawilliams <cawilliams@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2007-06-13 19:04:52 +0000

    add id of new ruby class wizard
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@2615 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.ui/src/org/rubypeople/rdt/ui/IRubyConstants.java
+++ org.rubypeople.rdt.ui/src/org/rubypeople/rdt/ui/IRubyConstants.java
@@ -4,7 +4,7 @@
 package org.rubypeople.rdt.ui;
 
 /**
- * Externally facing inetrface holding constants for various UI elements. Helps
+ * Externally facing interface holding constants for various UI elements. Helps
  * people extend our plugin!
  * 
  */
@@ -14,5 +14,6 @@ public interface IRubyConstants {
 	public static final String EDITOR_ID = "org.rubypeople.rdt.ui.EditorRubyFile"; //$NON-NLS-1$
 	public static final String EXTERNAL_FILES_EDITOR_ID = "org.rubypeople.rdt.ui.ExternalRubyEditor"; //$NON-NLS-1$
 	public static final String RI_VIEW_ID = "org.rubypeople.rdt.ui.views.RIView"; //$NON-NLS-1$
+	public static final String ID_NEW_CLASS_WIZARD = "org.rubypeople.rdt.ui.wizards.RubyNewClassWizard"; //$NON-NLS-1$
 
 }
