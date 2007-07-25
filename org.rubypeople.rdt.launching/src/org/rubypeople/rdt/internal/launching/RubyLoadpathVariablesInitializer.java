3d0a66949e5ed20b9275e73ad0f6d6c97eecfb3b
cawilliams <cawilliams@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2007-07-25 14:27:05 +0000

    fix #5299 - Make gem loadpaths use variables that get resolved. Gem loadpaths now use the RUBY_LIB variable to make paths relative to the ruby library directory. This makes it so that oaths aren't absolute anymore and can be moved across workspaces/etc.
    BUT, gems aren't always relative to the Ruby lib folder, so we really need to define a special GEM_LIB variable.
    
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@2837 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.launching/src/org/rubypeople/rdt/internal/launching/RubyLoadpathVariablesInitializer.java
+++ org.rubypeople.rdt.launching/src/org/rubypeople/rdt/internal/launching/RubyLoadpathVariablesInitializer.java
@@ -2,12 +2,16 @@ package org.rubypeople.rdt.internal.launching;
 
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.IWorkspaceDescription;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Path;
 import org.rubypeople.rdt.core.LoadpathVariableInitializer;
 import org.rubypeople.rdt.core.RubyCore;
+import org.rubypeople.rdt.launching.IVMInstall;
+import org.rubypeople.rdt.launching.RubyRuntime;
 
 public class RubyLoadpathVariablesInitializer extends LoadpathVariableInitializer {
 
@@ -15,38 +19,36 @@ public class RubyLoadpathVariablesInitializer extends LoadpathVariableInitialize
 
 	@Override
 	public void initialize(String variable) {
-//		 FIXME What should we be looking for?!
-//		IVMInstall vmInstall= RubyRuntime.getDefaultVMInstall();
-//		if (vmInstall != null) {
-//			IPath newPath= null;
-//			IPath[] locations= RubyRuntime.getLibraryLocations(vmInstall);
-//			
-//			
-//			if (systemLib != null) {
-//				if (variable.equals(RubyRuntime.RUBYLIB_VARIABLE)) {
-//					newPath= systemLib;
-//				} 
-//				if (newPath == null) {
-//					return;
-//				}
-//				IWorkspace workspace= ResourcesPlugin.getWorkspace();
-//				IWorkspaceDescription wsDescription= workspace.getDescription();				
-//				boolean wasAutobuild= wsDescription.isAutoBuilding();
-//				try {
-//					setAutobuild(workspace, false);
-//					setRubyVMVariable(newPath, variable);	
-//				} catch (CoreException ce) {
-//					LaunchingPlugin.log(ce);
-//					return;
-//				} finally {
-//					try {
-//						setAutobuild(workspace, wasAutobuild);
-//					} catch (CoreException ce) {
-//						LaunchingPlugin.log(ce);
-//					}
-//				}
-//			}
-//		}		
+		IVMInstall vmInstall= RubyRuntime.getDefaultVMInstall();
+		if (vmInstall != null) {
+			IPath newPath= null;
+//			IPath[] locations= RubyRuntime.getLibraryLocations(vmInstall);			
+			IPath systemLib = new Path(vmInstall.getInstallLocation().getAbsolutePath()).append("lib");
+			if (systemLib != null) {
+				if (variable.equals(RubyRuntime.RUBYLIB_VARIABLE)) {
+					newPath= systemLib;
+				} 
+				if (newPath == null) {
+					return;
+				}
+				IWorkspace workspace= ResourcesPlugin.getWorkspace();
+				IWorkspaceDescription wsDescription= workspace.getDescription();				
+				boolean wasAutobuild= wsDescription.isAutoBuilding();
+				try {
+					setAutobuild(workspace, false);
+					setRubyVMVariable(newPath, variable);	
+				} catch (CoreException ce) {
+					LaunchingPlugin.log(ce);
+					return;
+				} finally {
+					try {
+						setAutobuild(workspace, wasAutobuild);
+					} catch (CoreException ce) {
+						LaunchingPlugin.log(ce);
+					}
+				}
+			}
+		}		
 	}
 	
 	private void setRubyVMVariable(IPath newPath, String var) throws CoreException {
