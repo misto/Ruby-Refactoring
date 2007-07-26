94ea1596510775ee1e68db2ca6afbd33fda48707
cawilliams <cawilliams@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2007-07-26 15:02:42 +0000

    if no default vm is installed/chosen, add listener and wait until we get one, then initialize the gem manager
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@2884 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- com.aptana.rdt.ui/src/com/aptana/rdt/ui/RubyGemsInitializer.java
+++ com.aptana.rdt.ui/src/com/aptana/rdt/ui/RubyGemsInitializer.java
@@ -1,13 +1,38 @@
 package com.aptana.rdt.ui;
 
 import org.eclipse.ui.IStartup;
+import org.rubypeople.rdt.launching.IVMInstall;
+import org.rubypeople.rdt.launching.IVMInstallChangedListener;
+import org.rubypeople.rdt.launching.PropertyChangeEvent;
+import org.rubypeople.rdt.launching.RubyRuntime;
 
 import com.aptana.rdt.AptanaRDTPlugin;
 
-public class RubyGemsInitializer implements IStartup {
+public class RubyGemsInitializer implements IStartup, IVMInstallChangedListener {
 
 	public void earlyStartup() {
-		AptanaRDTPlugin.getDefault().getGemManager().initialize();
+		// If ruby isn't set up, listen until we have an interpreter
+		IVMInstall vm = RubyRuntime.getDefaultVMInstall();
+		if (vm == null) {
+			RubyRuntime.addVMInstallChangedListener(this);
+		} else {
+			AptanaRDTPlugin.getDefault().getGemManager().initialize();
+		}
+	}
+
+	public void defaultVMInstallChanged(IVMInstall previous, IVMInstall current) {
+		if (current != null && !AptanaRDTPlugin.getDefault().getGemManager().isInitialized()) {
+			AptanaRDTPlugin.getDefault().getGemManager().initialize();
+		}		
+	}
+
+	public void vmAdded(IVMInstall newVm) {		
+	}
+
+	public void vmChanged(PropertyChangeEvent event) {		
+	}
+
+	public void vmRemoved(IVMInstall removedVm) {		
 	}
 
 }
