5d39005fa1d162c3876a250514801f5a6f82b24f
cawilliams <cawilliams@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2007-09-06 15:44:39 +0000

    fix positioning of error
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@3096 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- com.aptana.rdt/src/com/aptana/rdt/internal/parser/warnings/ConstantNamingConvention.java
+++ com.aptana.rdt/src/com/aptana/rdt/internal/parser/warnings/ConstantNamingConvention.java
@@ -4,7 +4,6 @@ import org.jruby.ast.ConstDeclNode;
 import org.jruby.evaluator.Instruction;
 import org.jruby.lexer.yacc.IDESourcePosition;
 import org.jruby.lexer.yacc.ISourcePosition;
-import org.rubypeople.rdt.core.RubyCore;
 import org.rubypeople.rdt.core.parser.warnings.RubyLintVisitor;
 
 import com.aptana.rdt.AptanaRDTPlugin;
@@ -26,7 +25,7 @@ public class ConstantNamingConvention extends RubyLintVisitor {
 		String name = iVisited.getName();
 		if (!name.toUpperCase().equals(name)) {
 			ISourcePosition pos = iVisited.getPosition();
-			IDESourcePosition duh = new IDESourcePosition("", pos.getStartLine(), pos.getEndLine(), pos.getStartOffset(), pos.getStartOffset() + name.length() - 1);
+			IDESourcePosition duh = new IDESourcePosition("", pos.getStartLine(), pos.getEndLine(), pos.getStartOffset(), pos.getStartOffset() + name.length());
 			createProblem(duh, "Constant name doesn't match ALL_CAPS_WITH_UNDERSCORES convention: " + name);
 		}
 		return super.visitConstDeclNode(iVisited);
