0e1724d976b599da9c5fe0d20c51200dbab9c592
cawilliams <cawilliams@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2007-09-11 13:58:21 +0000

    fix #5911 - Mark occurrences doesn't mark rest args in method signature
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@3131 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.ui/src/org/rubypeople/rdt/internal/ui/search/OccurrencesFinder.java
+++ org.rubypeople.rdt.ui/src/org/rubypeople/rdt/internal/ui/search/OccurrencesFinder.java
@@ -701,7 +701,10 @@ public class OccurrencesFinder extends AbstractOccurencesFinder {
 
 		// TODO refactor the getting-of-name
 		String name = null;
-		if (node instanceof ReturnNode) {
+		if (node instanceof ArgumentNode) {
+			ArgumentNode argNode = (ArgumentNode) node;
+			name = argNode.getName();
+		} else if (node instanceof ReturnNode) {
 			return node.getPosition();
 		} else if (isLocalVarRef(node) || isDVarRef(node) || isInstanceVarRef(node) || isGlobalVarRef(node) || isClassVarRef(node) || isConstRef(node) || node instanceof BlockArgNode) {
 			name = ASTUtil.getNameReflectively(node);
