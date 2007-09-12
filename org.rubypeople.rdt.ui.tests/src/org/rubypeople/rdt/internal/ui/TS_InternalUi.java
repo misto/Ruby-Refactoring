a4fdc908128d9c2f1478339f1ef19ca932dd4c68
cawilliams <cawilliams@0bd42bcd-5f16-0410-9896-c9ec0b0ce843>
2007-09-12 17:44:25 +0000

    start removing the Ruby Resources View
    git-svn-id: https://rubyeclipse.svn.sourceforge.net/svnroot/rubyeclipse/trunk@3150 0bd42bcd-5f16-0410-9896-c9ec0b0ce843

--- org.rubypeople.rdt.ui.tests/src/org/rubypeople/rdt/internal/ui/TS_InternalUi.java
+++ org.rubypeople.rdt.ui.tests/src/org/rubypeople/rdt/internal/ui/TS_InternalUi.java
@@ -6,8 +6,7 @@ import junit.framework.TestSuite;
 public class TS_InternalUi {
 
 	public static Test suite() {
-		TestSuite suite = new TestSuite("org.rubypeople.rdt.internal.ui");		
-		suite.addTestSuite(TC_RubyViewerFilter.class);
+		TestSuite suite = new TestSuite("org.rubypeople.rdt.internal.ui");
 		suite.addTestSuite(TC_StackTraceLine.class);
 		suite.addTestSuite(TC_ResourceAdapterFactory.class);
 		suite.addTestSuite(TC_RubyFileMatcher.class);
