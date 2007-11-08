d92605d059d68e015f2af630331f1b0b540603a6
Mirko Stocker <me@misto.ch>
2007-11-08 09:43:35 +0100

    finally remove RDT from our repository

--- org.rubypeople.rdt.build/cruiseControl/stopCC.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-pgid=`ps -ef | grep [s]tartCC | awk '{print $2}'`
-kill `ps -eo pgid,pid | grep $pgid | awk '{print $2}'`
--- org.rubypeople.rdt.core/src/org/rubypeople/rdt/internal/core/parser/IRdtWarnings.java
+++
@@ -1,8 +0,0 @@
-package org.rubypeople.rdt.internal.core.parser;
-
-import org.eclipse.core.resources.IFile;
-import org.jruby.common.IRubyWarnings;
-
-public interface IRdtWarnings extends IRubyWarnings {
-    public void setFile(IFile file);
-}
